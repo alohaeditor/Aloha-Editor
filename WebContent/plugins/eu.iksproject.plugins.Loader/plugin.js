92b979ebdf21897c6b38ce999c2c97c767614959
Jerry Jalava <jerry.jalava@gmail.com>
2010-07-26 19:28:18 +0800

    Allow asset loading by type

--- WebContent/plugins/eu.iksproject.plugins.Loader/plugin.js
+++ WebContent/plugins/eu.iksproject.plugins.Loader/plugin.js
@@ -7,6 +7,7 @@ if (typeof eu == "undefined") {
 }
 
 eu.iksproject.LoaderPlugin = new GENTICS.Aloha.Plugin('eu.iksproject.plugins.Loader');
+eu.iksproject.LoaderPlugin.loadedAssets = {};
 
 /**
  * Initialize the plugin, register the buttons
@@ -15,13 +16,28 @@ eu.iksproject.LoaderPlugin.init = function() {
 	var that = this;
 };
 
-eu.iksproject.LoaderPlugin.load = function(pluginNamespace) {
+eu.iksproject.LoaderPlugin.load = function(pluginNamespace) {    
     var pluginPath = '' + GENTICS_Aloha_base + '/plugins/' + pluginNamespace + '/plugin.js';
 	jQuery('<script type="text/javascript" />').attr('src', pluginPath).appendTo('head');
 };
 
-eu.iksproject.LoaderPlugin.loadAsset = function(pluginNamespace, assetName) {
-	var assetPath = '' + GENTICS_Aloha_base + '/plugins/' + pluginNamespace + '/' + assetName;
-	jQuery('<script type="text/javascript" />').attr('src', assetPath).appendTo('head');
+eu.iksproject.LoaderPlugin.loadAsset = function(pluginNamespace, assetName, assetType) {
+    if (typeof assetType == 'undefined') {
+        assetType = 'js';
+    }
+    
+	var assetPath = '' + GENTICS_Aloha_base + '/plugins/' + pluginNamespace + '/' + assetName + '.' + assetType;
+    
+    if (this.loadedAssets[assetPath]) {
+        return;
+    }
+    
+    this.loadedAssets[assetPath] = true;
+    
+    if (assetType == 'js') {
+        jQuery('<script type="text/javascript" />').attr('src', assetPath).appendTo('head');
+    } else if (assetType == 'css') {
+        jQuery('<link rel="stylesheet" />').attr('href', assetPath).appendTo('head');
+    }
 };
 
