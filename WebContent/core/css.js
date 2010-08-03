8bab0912f2bb2a03a4e1ee4187e944b222051d4f
Norbert Pomaroli <n.pomaroli@gentics.com>
2010-08-03 09:44:33 +0200

    fixed inclusion of css files and core files for development environment

--- WebContent/core/css.js
+++ WebContent/core/css.js
@@ -1,10 +1,26 @@
+/* This is auto generated on build-dev-include build. Templates at /build/deps/includecss* */ 
 (function(){
+	
+	/* Check if base DIR is set through configuration otherwise set to default */
 	var base = GENTICS.Aloha.autobase;
 	if (typeof GENTICS_Aloha_base != 'undefined') {
 			base = GENTICS_Aloha_base;
 	}
-document.write('<link type="text/css" href="' + base + 'css/aloha.css?v=' + GENTICS.Aloha.version + '" rel="stylesheet" />');
-document.write('<link type="text/css" href="' + base + 'plugins/com.gentics.aloha.plugins.Table/resources/table.css?v=' + GENTICS.Aloha.version + '" rel="stylesheet" />');
-document.write('<link type="text/css" href="' + base + 'deps/extjs/resources/css/ext-all.css?v=' + GENTICS.Aloha.version + '" rel="stylesheet" />');
-document.write('<link type="text/css" href="' + base + 'deps/extjs/resources/css/xtheme-gray.css?v=' + GENTICS.Aloha.version + '" rel="stylesheet" />');
+
+	/* load stylsheets */
+	var header = document.getElementsByTagName("head")[0];
+	header.appendChild(cssElement(base + 'css/aloha.css?v=' + GENTICS.Aloha.version));
+	header.appendChild(cssElement(base + 'plugins/com.gentics.aloha.plugins.Table/resources/table.css?v=' + GENTICS.Aloha.version));
+	header.appendChild(cssElement(base + 'deps/extjs/resources/css/ext-all.css?v=' + GENTICS.Aloha.version));
+	header.appendChild(cssElement(base + 'deps/extjs/resources/css/xtheme-gray.css?v=' + GENTICS.Aloha.version));
+	
+	/* Generate xHTML valid css Elements */
+	function cssElement( link ) {
+		var csslink = document.createElement("link");
+		csslink.setAttribute("rel", "stylesheet");
+		csslink.setAttribute("type", "text/css");
+		csslink.setAttribute("href", link);
+		csslink.setAttribute("media", "all");
+		return csslink;
+	}
 })();
