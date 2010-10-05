ae8ff34a8c9fa4ec8c3301f890a7c84e0f9c57b6
Clemens Prerovsky <c.prerovsky@gentics.com>
2010-10-05 14:51:28 +0200

    added page repository for GCN integration

---
+++ WebContent/plugins/com.gentics.aloha.plugins.GCN/page.js
@@ -0,0 +1,31 @@
+/**
+ * Create the resource object & check for resource namespace
+ * @hide
+ */
+if ( !GENTICS.Aloha.Repositories ) GENTICS.Aloha.Repositories = {};
+GENTICS.Aloha.Repositories.Page = new GENTICS.Aloha.Repository('com.gentics.aloha.GCN.Page');
+
+/**
+ * Searches a resource for resource items matching query if objectTypes.
+ * If none found it returns null.
+ */
+GENTICS.Aloha.Repositories.Page.query = function(queryString, objectTypeFilter, filter, inFolderId, orderBy, maxItems, skipCount, renditionFilter, callback) {
+	var that = this;
+	jQuery.ajax({ type: "POST",
+		url: "/Portal.Node/portal?gentics.rs=search.Search&gentics.rsid=jsonsearch&gentics.pb=center",
+		dataType : "json",
+		data : {
+			q : queryString + "*" // always query for page name + wildcard
+		},
+		success: function(data) {
+			callback.call( that, data.pages);
+		}
+	});
+};
+
+/**
+ * callback after a product has been selected from the resource
+ */
+//GENTICS.Aloha.Repositories.Page.markObject = function (obj, resourceItem) {
+	
+//};
