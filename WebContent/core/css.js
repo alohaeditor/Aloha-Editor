d8f3543da3267a9dff98c6163c824307af2d717e
Haymo Meran <h.meran@gentics.com>
2010-10-27 15:21:02 +0200

    Moved table styles to example. They are not part of the plugin.

--- WebContent/core/css.js
+++ WebContent/core/css.js
@@ -16,7 +16,6 @@
 		header.appendChild(cssElement(base + 'deps/extjs/resources/css/xtheme-gray.css?v=' + GENTICS.Aloha.version));
 		header.appendChild(cssElement(base + 'deps/prettyPhoto/resources/css/prettyPhoto.css?v=' + GENTICS.Aloha.version));
 		header.appendChild(cssElement(base + 'plugins/com.gentics.aloha.plugins.Table/resources/table.css?v=' + GENTICS.Aloha.version));
-		header.appendChild(cssElement(base + 'plugins/com.gentics.aloha.plugins.Table/resources/table-styles.css?v=' + GENTICS.Aloha.version));
 		header.appendChild(cssElement(base + 'plugins/com.gentics.aloha.plugins.Link/css/Link.css?v=' + GENTICS.Aloha.version));
 		header.appendChild(cssElement(base + 'plugins/com.gentics.aloha.plugins.HighlightEditables/css/HighlightEditables.css?v=' + GENTICS.Aloha.version));
 
