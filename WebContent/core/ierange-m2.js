c1e5df3ce1a69c73f7d78661b159a2f5b84b1b19
Haymo Meran <h.meran@gentics.com>
2010-11-05 16:32:24 +0100

    changed license information.

--- WebContent/core/ierange-m2.js
+++ WebContent/core/ierange-m2.js
@@ -1,16 +1,35 @@
-/**
- * Only execute the following code if we are in IE (check for
- * document.attachEvent, this is a microsoft event and therefore only available
- * in IE).
- */
+/*!
+*   This file is part of Aloha Editor
+*   Author & Copyright (c) 2010 Gentics Software GmbH, aloha@gentics.com
+*   Licensed unter the terms of http://www.aloha-editor.com/license.html
+*//*
+*	Aloha Editor is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU Affero General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.*
+*
+*   Aloha Editor is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU Affero General Public License for more details.
+*
+*   You should have received a copy of the GNU Affero General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*
+*
+*   Only execute the following code if we are in IE (check for
+*   document.attachEvent, this is a microsoft event and therefore only available
+*   in IE).
+*/
 if(document.attachEvent && document.selection) {
-	/*
-	  DOM Ranges for Internet Explorer (m2)
-	  
-	  Copyright (c) 2009 Tim Cameron Ryan
-	  Released under the MIT/X License
-	  available at http://code.google.com/p/ierange/
-	 */
+/*!
+*   DOM Ranges for Internet Explorer (m2)
+*	  
+*   Copyright (c) 2009 Tim Cameron Ryan
+*   Released under the MIT/X License
+*   available at http://code.google.com/p/ierange/
+*/
 	 
 	/*
 	  Range reference:
