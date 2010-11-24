79dae8c2f3e926d1d1df673a2ed5d9432244a655
Haymo Meran <h.meran@gentics.com>
2010-11-24 12:46:24 +0100

    Fixed title warning.

--- plugin.js
+++ plugin.js
@@ -17,7 +17,7 @@ GENTICS.Aloha.Format.languages = ['en', 'de', 'fr', 'eo', 'fi', 'ru', 'it', 'pl'
 /**
  * default button configuration
  */
-GENTICS.Aloha.Format.config = [ 'b', 'i','del','sub','sup', 'p', 'title', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'pre', 'removeFormat'];
+GENTICS.Aloha.Format.config = [ 'b', 'i','del','sub','sup', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'pre', 'removeFormat'];
 
 /**
  * Initialize the plugin and set initialize flag on true
