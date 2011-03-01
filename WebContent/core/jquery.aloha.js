f0fe6b52ca88842e0d3975c513606d4bbbe610e7
Thomas <bistory@gmail.com>
2011-03-01 11:21:56 +0100

    Fixed a bug on the bundled version for use in real-world. And ensure jQuery.fn.aloha is in a closure.

--- WebContent/core/jquery.aloha.js
+++ WebContent/core/jquery.aloha.js
@@ -17,100 +17,101 @@
 *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+(function(jQuery) {
+	/**
+	 * jQuery Aloha Plugin
+	 *
+	 * turn all dom elements to continous text
+	 * @return	jQuery object for the matched elements
+	 * @api
+	 */
+	jQuery.fn.aloha = function() {
+		return this.GENTICS_aloha();
+	};
 
-/**
- * jQuery Aloha Plugin
- *
- * turn all dom elements to continous text
- * @return	jQuery object for the matched elements
- * @api
- */
-jQuery.fn.aloha = function() {
-	return this.GENTICS_aloha();
-};
+	/**
+	 * namespaced fallback for aloha jQuery plugin
+	 *
+	 * turn all dom elements to continous text
+	 * @return	jQuery object for the matched elements
+	 * @api
+	 */
+	jQuery.fn.GENTICS_aloha = function() {
+		return this.each(function() {
+			// create a new aloha editable object for each queried object
+			if (!GENTICS.Aloha.isEditable(this)) {
+				new GENTICS.Aloha.Editable(jQuery(this));
+			}
+		});
+	};
 
-/**
- * namespaced fallback for aloha jQuery plugin
- *
- * turn all dom elements to continous text
- * @return	jQuery object for the matched elements
- * @api
- */
-jQuery.fn.GENTICS_aloha = function() {
-	return this.each(function() {
-		// create a new aloha editable object for each queried object
-		if (!GENTICS.Aloha.isEditable(this)) {
-			new GENTICS.Aloha.Editable(jQuery(this));
-		}
-	});
-};
+	/**
+	 * jQuery destroy elements as editable
+	 *
+	 * destroy all mached elements editable capabilities
+	 * @return	jQuery object for the matched elements
+	 * @api
+	 */
+	jQuery.fn.mahalo = function() {
+		return this.GENTICS_mahalo();
+	};
 
-/**
- * jQuery destroy elements as editable
- *
- * destroy all mached elements editable capabilities
- * @return	jQuery object for the matched elements
- * @api
- */
-jQuery.fn.mahalo = function() {
-	return this.GENTICS_mahalo();
-};
+	/**
+	 * namespaced fallback for jQuery destroy elements as editable
+	 *
+	 * destroy all mached elements editable capabilities
+	 * @return	jQuery object for the matched elements
+	 * @api
+	 */
+	jQuery.fn.GENTICS_mahalo = function() {
+		return this.each(function() {
+			if (GENTICS.Aloha.isEditable(this)) {
+				GENTICS.Aloha.getEditableById(jQuery(this).attr('id')).destroy();
+			}
+		});
+	};
 
-/**
- * namespaced fallback for jQuery destroy elements as editable
- *
- * destroy all mached elements editable capabilities
- * @return	jQuery object for the matched elements
- * @api
- */
-jQuery.fn.GENTICS_mahalo = function() {
-	return this.each(function() {
-		if (GENTICS.Aloha.isEditable(this)) {
-			GENTICS.Aloha.getEditableById(jQuery(this).attr('id')).destroy();
-		}
-	});
-};
+	/**
+	 * jQuery Extension
+	 * new Event which is triggered whenever a selection (length >= 0) is made in
+	 * an Aloha Editable element
+	 */
+	jQuery.fn.GENTICS_contentEditableSelectionChange = function(callback) {
+		var that = this;
 
-/**
- * jQuery Extension
- * new Event which is triggered whenever a selection (length >= 0) is made in
- * an Aloha Editable element
- */
-jQuery.fn.GENTICS_contentEditableSelectionChange = function(callback) {
-	var that = this;
-
-	// update selection when keys are pressed
-	this.keyup(function(event){
-		var rangeObject = GENTICS.Aloha.Selection.getRangeObject();
-		callback(event);
-	});
-
-	// update selection on doubleclick (especially important for the first automatic selection, when the Editable is not active yet, but is at the same time activated as the selection occurs
-	this.dblclick(function(event) {
-		callback(event);
-	});
-
-	// update selection when text is selected
-	this.mousedown(function(event){
-		// remember that a selection was started
-		that.selectionStarted = true;
-	});
-	jQuery(document).mouseup(function(event) {
-		GENTICS.Aloha.Selection.eventOriginalTarget = that;
-		if (that.selectionStarted) {
+		// update selection when keys are pressed
+		this.keyup(function(event){
+			var rangeObject = GENTICS.Aloha.Selection.getRangeObject();
 			callback(event);
+		});
+
+		// update selection on doubleclick (especially important for the first automatic selection, when the Editable is not active yet, but is at the same time activated as the selection occurs
+		this.dblclick(function(event) {
+			callback(event);
+		});
+
+		// update selection when text is selected
+		this.mousedown(function(event){
+			// remember that a selection was started
+			that.selectionStarted = true;
+		});
+		jQuery(document).mouseup(function(event) {
+			GENTICS.Aloha.Selection.eventOriginalTarget = that;
+			if (that.selectionStarted) {
+				callback(event);
+			}
+			GENTICS.Aloha.Selection.eventOriginalTarget = false;
+			that.selectionStarted = false;
+		});
+
+		return this;
+	};
+
+	jQuery.fn.outerHTML = function(s) {
+		if (s) {
+			return this.before(s).remove();
+		} else {
+			return jQuery('<p>').append(this.eq(0).clone()).html();
 		}
-		GENTICS.Aloha.Selection.eventOriginalTarget = false;
-		that.selectionStarted = false;
-	});
-
-	return this;
-};
-
-jQuery.fn.outerHTML = function(s) {
-	if (s) {
-		return this.before(s).remove();
-	} else {
-		return jQuery('<p>').append(this.eq(0).clone()).html();
-	}
-};
+	};
+})(jQuery);
