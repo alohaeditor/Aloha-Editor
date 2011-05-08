f267c67c58e361e1a366ab0cd322542899a7d132
nka11 <nka11@nka.me>
2011-05-08 16:44:26 +0200

    New-style refactoring

--- src/highlighteditables.js
+++ src/highlighteditables.js
@@ -5,66 +5,74 @@
 * Licensed unter the terms of http://www.aloha-editor.com/license.html
 */
 
-GENTICS.Aloha.HighlightEditables = new GENTICS.Aloha.Plugin('highlighteditables');
 
-/**
- * Initialize the plugin and set initialize flag on true
- */
-GENTICS.Aloha.HighlightEditables.init = function () {
+(function(window, undefined) {
+	var
+		jQuery = window.alohaQuery, $ = jQuery,
+		GENTICS = window.GENTICS,
+		Aloha = window.Aloha;
 
-	// remember refernce to this class for callback
-	var that = this;
+	
+Aloha.HilightEditable = new (Aloha.Plugin.extend({ 
+	_constructor: function(){
+		this._super('highlighteditables');
+	},
+	
+	init: function () {
 
-	// highlight editables as long as the mouse is moving
-	GENTICS.Utils.Position.addMouseMoveCallback(function () {
-		for ( var i = 0; i < GENTICS.Aloha.editables.length; i++) {
-			var editable = GENTICS.Aloha.editables[i];
-			if (!GENTICS.Aloha.activeEditable && !editable.isDisabled()) {
-				editable.obj.addClass('GENTICS_editable_highlight');
+		// remember refernce to this class for callback
+		var that = this;
+
+		// highlight editables as long as the mouse is moving
+		GENTICS.Utils.Position.addMouseMoveCallback(function () {
+			for ( var i = 0; i < Aloha.editables.length; i++) {
+				var editable = Aloha.editables[i];
+				if (!Aloha.activeEditable && !editable.isDisabled()) {
+					editable.obj.addClass('GENTICS_editable_highlight');
+				}
 			}
-		}
-	});
+		});
 
-	// fade editable borders when mouse stops moving
-	GENTICS.Utils.Position.addMouseStopCallback(function () {
-		that.fade();
-	});
+		// fade editable borders when mouse stops moving
+		GENTICS.Utils.Position.addMouseStopCallback(function () {
+			that.fade();
+		});
 
-	// mark active Editable with a css class
-	GENTICS.Aloha.EventRegistry.subscribe(
-			GENTICS.Aloha,
-			"editableActivated",
-			function (jEvent, aEvent) {
-				aEvent.editable.obj.addClass('GENTICS_editable_active');
-				that.fade();
+		// mark active Editable with a css class
+		Aloha.bind(
+				"aloha-editable-activated",
+				function (jEvent, aEvent) {
+					aEvent.editable.obj.addClass('GENTICS_editable_active');
+					that.fade();
+				}
+		);
+
+		// remove active Editable ccs class
+		Aloha.bind("aloha-editable-deactivated",
+				function (jEvent, aEvent) {
+					aEvent.editable.obj.removeClass('GENTICS_editable_active');
+				}
+		);
+
+	},
+	/**
+	 * fades all highlighted editables
+	 */
+	fade: function () {
+		for ( var i = 0; i < Aloha.editables.length; i++) {
+			var editable = Aloha.editables[i].obj;
+			if (editable.hasClass('GENTICS_editable_highlight')) {
+				editable.removeClass('GENTICS_editable_highlight')
+					.css('outline', '5px solid #FFE767')
+					.animate({
+						outlineWidth : '0px'
+					}, 300, 'swing', function () {
+						jQuery(this).css('outline', '');
+					});
 			}
-	);
-
-	// remove active Editable ccs class
-	GENTICS.Aloha.EventRegistry.subscribe(
-			GENTICS.Aloha,
-			"editableDeactivated",
-			function (jEvent, aEvent) {
-				aEvent.editable.obj.removeClass('GENTICS_editable_active');
-			}
-	);
-
-};
-
-/**
- * fades all highlighted editables
- */
-GENTICS.Aloha.HighlightEditables.fade = function () {
-	for ( var i = 0; i < GENTICS.Aloha.editables.length; i++) {
-		var editable = GENTICS.Aloha.editables[i].obj;
-		if (editable.hasClass('GENTICS_editable_highlight')) {
-			editable.removeClass('GENTICS_editable_highlight')
-				.css('outline', '5px solid #FFE767')
-				.animate({
-					outlineWidth : '0px'
-				}, 300, 'swing', function () {
-					jQuery(this).css('outline', '');
-				});
 		}
 	}
-};
+	
+}))();
+})(window);
+
