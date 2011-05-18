76ac7465ff78fca9ed755561203617dac5c440f7
Benjamin Lupton <b@lupton.cc>
2011-05-19 00:23:09 +1000

    Built aloha for nico as he can't build. ref #94

--- src/aloha.js
+++ src/aloha.js
@@ -1,3 +1,6 @@
+// Namespace jQuery
+window.alohaQuery = window.jQuery.sub();
+
 // Ensure Namespace
 window.GENTICS = window.GENTICS || {};
 window.GENTICS.Utils = window.GENTICS.Utils || {};
@@ -9,7 +12,7 @@ window.Aloha_pluginDir = window.Aloha_pluginDir || false;
 window.Aloha_base = window.Aloha_base || false;
 
 /* This is auto generated on build-dev-include build. Templates at /build/deps/includejs* */
-(function(window,document) {
+(function(window, undefined) {
 
 	// Prepare Script Loading
 	var
@@ -64,46 +67,41 @@ window.Aloha_base = window.Aloha_base || false;
 		
 		// Prepare baseUrl
 		window.GENTICS_Aloha_base = window.GENTICS_Aloha_base || document.getElementById('aloha-script-include').src.replace(/aloha\.js$/,'').replace(/\/+$/,'');
-		
 		// Prepare Plugin Loading
-		window.Aloha_loaded_plugins = window.Aloha_loaded_plugins||[];
-		window.Aloha_loaded_plugins['format'] = true;
-		
-		
-		includes.push('core/repositoryobjects.js');
-		includes.push('core/repository.js');
-		includes.push('core/repositorymanager.js');
-		includes.push('core/sidebar.js');
-		includes.push('core/selection.js');
-		includes.push('core/plugin.js');
-		includes.push('core/message.js');
-		includes.push('core/markup.js');
-		includes.push('core/log.js');
-		includes.push('core/ierange-m2.js');
-		includes.push('core/floatingmenu.js');
-		includes.push('core/editable.js');
-		includes.push('core/ui-browser.js');
-		includes.push('core/ui-attributefield.js');
-		includes.push('core/ui.js');
-		includes.push('core/core.js');
-		includes.push('core/ext-alohatreeloader.js');
-		includes.push('core/ext-alohareader.js');
-		includes.push('core/ext-alohaproxy.js');
-		includes.push('util/dom.js');
-		includes.push('util/position.js');
-		includes.push('util/range.js');
-		includes.push('util/lang.js');
-		includes.push('core/jquery.aloha.js');
-		includes.push('dep/jquery.store.js');
-		includes.push('dep/jquery.getUrlParam.js');
-		includes.push('dep/jquery.json-2.2.min.js');
-		includes.push('dep/ext-3.2.1/ext-all.js');
-		includes.push('dep/ext-3.2.1/adapter/jquery/ext-jquery-adapter.js');
-		includes.push('util/base.js');
-		
-		
-		//Initialize event loop
-		loadJsFileAtIncludesCounter();
-	});
-
-})(window,document);
+		window.Aloha_loaded_plugins = window.Aloha_loaded_plugins||[];	window.Aloha_loaded_plugins['format'] = true;
+	includes.push('util/base.js');
+	includes.push('dep/ext-3.2.1/adapter/jquery/ext-jquery-adapter.js');
+	includes.push('dep/ext-3.2.1/ext-all.js');
+	includes.push('dep/jquery.json-2.2.min.js');
+	includes.push('dep/jquery.getUrlParam.js');
+	includes.push('dep/jquery.store.js');
+	includes.push('core/jquery.aloha.js');
+	includes.push('util/lang.js');
+	includes.push('util/range.js');
+	includes.push('util/position.js');
+	includes.push('util/dom.js');
+	includes.push('core/ext-alohaproxy.js');
+	includes.push('core/ext-alohareader.js');
+	includes.push('core/ext-alohatreeloader.js');
+	includes.push('core/core.js');
+	includes.push('core/ui.js');
+	includes.push('core/ui-attributefield.js');
+	includes.push('core/ui-browser.js');
+	includes.push('core/editable.js');
+	includes.push('core/floatingmenu.js');
+	includes.push('core/ierange-m2.js');
+	includes.push('core/log.js');
+	includes.push('core/markup.js');
+	includes.push('core/message.js');
+	includes.push('core/plugin.js');
+	includes.push('core/selection.js');
+	includes.push('core/sidebar.js');
+	includes.push('core/repositorymanager.js');
+	includes.push('core/repository.js');
+	includes.push('core/repositoryobjects.js');
+	includes.push('plugin/format/src/format.js');
+	includes = includes.reverse();
+	loadJsFileAtIncludesCounter();
+}); // DOM event
+// </closure>
+})(window);
