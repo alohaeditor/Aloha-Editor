d05f6e20891e30ca033733f61ad1838fd8599335
Benjamin Lupton <b@lupton.cc>
2011-05-24 19:05:35 +1000

    Fixed core load in non webkit browsers

--- template/src_bundle_footer.js
+++ template/src_bundle_footer.js
@@ -1,8 +1,10 @@
-
 	// Variables
 	var
 		// jQuery
 		jQuery = window.alohaQuery, $ = jQuery,
+		// Loading
+		webkit = /WebKit/.test(navigator.userAgent),
+		scriptEls = [],
 		// Async
 		completed = 0,
 		total = includes.length,
@@ -22,25 +24,40 @@
 					exited = true;
 					next();
 				}
+				else if ( !webkit ) {
+					appendEl.appendChild(scriptEls[completed]);
+				}
 			}
 		},
 		// Loop
 		value, url, scriptEl, appendEl = document.head || document.getElementsByTagName('head')[0];
-
+	
 	// Insert Scripts
 	for ( i=0,n=total; i<n; ++i ) {
 		// Prepare
 		value = includes[i];
 		url = window.GENTICS_Aloha_base + '/' + value;
 
-		// Append via DOM
+		// Create
 		scriptEl = document.createElement('script');
 		scriptEl.src = url;
 		scriptEl.setAttribute('defer','defer');
 		scriptEl.onreadystatechange = scriptLoaded;
 		scriptEl.onload = scriptLoaded;
 		scriptEl.onerror = scriptLoaded;
-		appendEl.appendChild(scriptEl);
+
+		// Add
+		if ( webkit ) {
+			appendEl.appendChild(scriptEl);
+		}
+		else {
+			scriptEls.push(scriptEl);
+		}
+	}
+
+	// Non-Webkit
+	if ( !webkit ) {
+		appendEl.appendChild(scriptEls[0]);
 	}
 
 // </closure>
