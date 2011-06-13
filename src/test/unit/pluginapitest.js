677d57912bbcff2b0d984f21a1d087d8ffd4f232
nka11 <nka11@nka.me>
2011-06-13 12:10:01 +0200

    Adding unit test for #129.
    A uncomplete or bogus plugin should only be reported in error log, and should'nt block aloha loading as it does now.

---
+++ src/test/unit/pluginapitest.js
@@ -0,0 +1,64 @@
+/*!
+ * This file is part of Aloha Editor
+ * Author & Copyright (c) 2010 Gentics Software GmbH, aloha@gentics.com
+ * Licensed unter the terms of http://www.aloha-editor.com/license.html
+ */
+
+Aloha.settings = {
+	logLevels : {
+		'error': true,
+		'warn':  true,
+		'info':  false,
+		'debug': false
+	},
+	logHistory : {
+		levels : {
+			'error' : true,
+			'warn' : true,
+			'info' : false,
+			'debug' : false
+		}
+	},
+	errorhandling : true
+};
+
+alohaQuery(document).ready(function($) {
+
+	test('.aloha() is loading', function() {
+		var editable = alohaQuery('#edit');
+		editable.aloha();
+		equals(editable.size(),1, 'Editable was found');
+		ok(true, '.aloha() could be called');
+	});
+
+	// Test whether Aloha is properly initialized
+	asyncTest('Aloha Startup Test', function() {
+		$('body').bind('aloha',function() {
+			ok(true, 'Aloha Event was fired');
+			start();
+		});
+		setTimeout(function() {
+			ok(false, 'Aloha was not initialized within 60 seconds');
+			start();
+		}, 5000);
+	});
+
+	// All other tests are done when Aloha is ready
+	$('body').bind('aloha', function() {
+		// check whether error or warn messages were logged during startup
+		test('Aloha Error Log Test', function() {
+			var logHistory = Aloha.Log.getLogHistory();
+			equal(logHistory.length, 0, 'Check number of logged messages');
+		});
+
+		// check whether alohafying of divs works
+		test('Aloha Editable Test', function() {
+			var editable = $('#edit');
+			editable.aloha();
+			equals(editable.contentEditable(), "true", 'Check whether div is contenteditable after .aloha()');
+			editable.mahalo();
+			equals(editable.contentEditable(), "false", 'Check whether div is not contenteditable after .mahalo()');
+		});
+	});
+
+});
