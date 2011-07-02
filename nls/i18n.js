3290a62324a0987781a62f6d898d535a7443e800
Sebastian Kurfuerst <sebastian@typo3.org>
2011-07-02 15:56:46 +0200

    i18n transformation

---
+++ nls/i18n.js
@@ -0,0 +1,10 @@
+define({
+	root: {"floatingmenu.tab.table":"Table","deleterows.confirm":"Do you really want to delete the selected rows?","deletecolumns.confirm":"Do you really want to delete the selected columns?","deletetable.confirm":"Do you really want to delete the table?","Table":"Table","button.createtable.tooltip":"Insert Table","button.addcolleft.tooltip":"Add Column left","button.addcolright.tooltip":"Add Column right","button.delcols.tooltip":"Delete Columns","button.addrowbefore.tooltip":"Add Row before","button.addrowafter.tooltip":"Add Row after","button.delrows.tooltip":"Delete Rows","button.caption.tooltip":"Table caption","empty.caption":"Table caption"}
+,	"de":true,
+	"eo":true,
+	"fi":true,
+	"fr":true,
+	"it":true,
+	"pl":true,
+	"ru":true
+});
