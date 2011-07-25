a0faea3a3438660abdeaf38ef029f6d087ab76d5
Johannes Schüth <j.schueth@gentics.com>
2011-07-25 14:23:46 +0200

    Fixed JSON is undefined error when using Internet Explorer

--- src/lib/aloha.js
+++ src/lib/aloha.js
@@ -3,7 +3,7 @@
 * Copyright (c) 2010-2011 Gentics Software GmbH, aloha@gentics.com
 * Contributors http://aloha-editor.org/contribution.php 
 * Licensed unter the terms of http://www.aloha-editor.org/license.html
-*//*
+*
 * Aloha Editor is free software: you can redistribute it and/or modify
 * it under the terms of the GNU Affero General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
@@ -30,6 +30,7 @@ define(
 	'aloha',
 	[
 		'aloha/jquery',
+		'order!util/json2',
 		'order!vendor/jquery.json-2.2.min',
 		'order!vendor/jquery.store',
 		'order!util/base',
