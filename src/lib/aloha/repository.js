7e62a1c9c205b03dbb730dcda501dac186eeb340
Clemens Prerovsky <c.prerovsky@gentics.com>
2011-08-02 17:13:32 +0200

    satisfied pull request #180 by jotschi

--- src/lib/aloha/repository.js
+++ src/lib/aloha/repository.js
@@ -1,9 +1,9 @@
 /*!
 * This file is part of Aloha Editor Project http://aloha-editor.org
-* Copyright © 2010-2011 Gentics Software GmbH, aloha@gentics.com
+* Copyright (c) 2010-2011 Gentics Software GmbH, aloha@gentics.com
 * Contributors http://aloha-editor.org/contribution.php 
 * Licensed unter the terms of http://www.aloha-editor.org/license.html
-*//*
+*
 * Aloha Editor is free software: you can redistribute it and/or modify
 * it under the terms of the GNU Affero General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
@@ -19,7 +19,9 @@
 */
 
 define(
-['aloha/jquery'],
+[
+ 'aloha/jquery'
+ ],
 function(jQuery, undefined) {
 	"use strict";
 	
