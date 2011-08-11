731d7fd0584979737c210c7ba51fd62a72923b08
Norbert Pomaroli <n.pomaroli@gentics.com>
2011-08-11 18:02:43 +0200

    More advanced version of the require.js plugin to require aloha plugins. Contains more or less the same code, that is used in core.js (method loadPlugins) to load the initial plugins. Still does not fix some major problems, like initialization of plugins (when they are not loaded by configuration as well). #ref 165

--- src/lib/aloha-plugin.js
+++ src/lib/aloha-plugin.js
@@ -21,19 +21,59 @@
 (function () {
 	
 	define({
-	    load: function (name, req, load, config) {
-		
-			var tmp = name.split('/');
-			var bundleName = tmp[0];
-			var pluginName = tmp[1];
-			// TODO this is currently very ugly and should be fixed. At this
-			// point its not possible to fetch the aloha settings
-			var pluginFile = "../../../../plugins/" + bundleName + "/" + pluginName +"/lib/" + pluginName + "-plugin.js";
-			
-			name = pluginFile;
-		    req([name], function (value) {
-		    	load(value);
-	        });
-	    }
+	    load: function (name, require, load, config) {
+			var tmp = name.split('/'),
+				bundleName = tmp[0],
+				pluginName = tmp[1],
+				bundlePath = '',
+				paths = {},
+				oldpaths = undefined,
+				nonLibPaths = ['nls', 'css', 'vendor', 'res'], i;
+
+			// Background: We do not use CommonJS packages for our Plugins
+			// as this breaks the loading order when these modules have
+			// other dependencies.
+			// We "emulate" the commonjs modules with the path mapping.
+			/* require(
+			 *  { paths: {
+			 *      'format': 'plugins/common/format/lib',
+			 *      'format/nls': 'plugins/common/format/nls',
+			 *      ... for every nonLibPath ...
+			 *    }
+			 *  },
+			 *  ['format/format-plugin'],
+			 *  load <-- when everything is loaded, we continue
+			 */
+
+			// make sure, the config exists and has paths
+            config = config || {};
+            config.paths = config.paths || {};
+
+			if (Aloha.settings.basePath) {
+				bundlePath = Aloha.settings.basePath;
+			}
+
+			if (Aloha.settings.bundles && Aloha.settings.bundles[bundleName]) {
+				bundlePath += Aloha.settings.bundles[bundleName];
+			} else {
+				bundlePath += '../plugins/' + bundleName;
+			}
+
+			config.paths[pluginName] = bundlePath + '/' + pluginName + '/lib';
+
+			// As the "nls" path lies NOT inside /lib/, but is a sibling to /lib/, we need
+			// to register it explicitely. The same goes for the "css" folder.
+			for ( i = 0; i < nonLibPaths; i++) {
+				config.paths[pluginName + '/' + nonLibPaths[i]] = bundlePath + '/' + pluginName + '/' + nonLibPaths[i];
+			}
+
+			// now require the plugin
+		    require(
+		    	[pluginName + '/' + pluginName + '-plugin'],
+		    	function (value) {
+		    		load(value);
+		    	}
+		    );
+		}
 	});	
 }());
