29b9223949ac14f6c309e3f4d8be26c63adf7cf3
draftkraft <h.meran@gentics.com>
2011-08-26 09:01:45 +0200

    Added commands plugin

---
+++ commands/lib/commands-plugin.js
@@ -0,0 +1,16 @@
+/*!
+* Aloha Editor
+* Author & Copyright (c) 2010 Gentics Software GmbH
+* aloha-sales@gentics.com
+* Licensed unter the terms of http://www.aloha-editor.com/license.html
+*/
+
+
+define(
+['aloha/commands', 'commands/inserthtml'],
+function(Commands) {
+	"use strict";
+	
+	// do some config here
+		
+});
