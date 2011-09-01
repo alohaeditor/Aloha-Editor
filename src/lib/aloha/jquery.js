65a170eda75ded32a7f048520a341933ea4cb63d
Christopher Supnig <c.supnig@gentics.com>
2011-09-01 15:15:37 +0200

    added a missing sanity check to jquery that is fixing a crash in IE

--- src/lib/aloha/jquery.js
+++ src/lib/aloha/jquery.js
@@ -27,7 +27,92 @@ define(
 		if ( window.alohaQuery ) {
 			return window.alohaQuery;
 		}
-		
+		//PATCH FOR A JQUERY BUG IN 1.6.1 & 1.6.2
+		loadedJQuery.data = (function(jQuery){
+		return function( elem, name, data, pvt /* Internal Use Only */ ) {
+			if ( !jQuery.acceptData( elem ) ) {
+				return;
+			}
+			
+			var internalKey = jQuery.expando, getByName = typeof name === "string", thisCache,
+
+				// We have to handle DOM nodes and JS objects differently because IE6-7
+				// can't GC object references properly across the DOM-JS boundary
+				isNode = elem.nodeType,
+
+				// Only DOM nodes need the global jQuery cache; JS object data is
+				// attached directly to the object so GC can occur automatically
+				cache = isNode ? jQuery.cache : elem,
+
+				// Only defining an ID for JS objects if its cache already exists allows
+				// the code to shortcut on the same path as a DOM node with no cache
+				id = isNode ? elem[ jQuery.expando ] : elem[ jQuery.expando ] && jQuery.expando;
+
+			// Avoid doing any more work than we need to when trying to get data on an
+			// object that has no data at all
+			//if ( (!id || (pvt && id && !cache[ id ][ internalKey ])) && getByName && data === undefined ) {
+			if ( (!id || (pvt && id && (!cache[id] || !cache[ id ][ internalKey ]))) && getByName && data === undefined ) {
+				return;
+			}
+
+			if ( !id ) {
+				// Only DOM nodes need a new unique ID for each element since their data
+				// ends up in the global cache
+				if ( isNode ) {
+					elem[ jQuery.expando ] = id = ++jQuery.uuid;
+				} else {
+					id = jQuery.expando;
+				}
+			}
+
+			if ( !cache[ id ] ) {
+				cache[ id ] = {};
+
+				// TODO: This is a hack for 1.5 ONLY. Avoids exposing jQuery
+				// metadata on plain JS objects when the object is serialized using
+				// JSON.stringify
+				if ( !isNode ) {
+					cache[ id ].toJSON = jQuery.noop;
+				}
+			}
+
+			// An object can be passed to jQuery.data instead of a key/value pair; this gets
+			// shallow copied over onto the existing cache
+			if ( typeof name === "object" || typeof name === "function" ) {
+				if ( pvt ) {
+					cache[ id ][ internalKey ] = jQuery.extend(cache[ id ][ internalKey ], name);
+				} else {
+					cache[ id ] = jQuery.extend(cache[ id ], name);
+				}
+			}
+
+			thisCache = cache[ id ];
+
+			// Internal jQuery data is stored in a separate object inside the object's data
+			// cache in order to avoid key collisions between internal data and user-defined
+			// data
+			if ( pvt ) {
+				if ( !thisCache[ internalKey ] ) {
+					thisCache[ internalKey ] = {};
+				}
+
+				thisCache = thisCache[ internalKey ];
+			}
+
+			if ( data !== undefined ) {
+				thisCache[ jQuery.camelCase( name ) ] = data;
+			}
+
+			// TODO: This is a hack for 1.5 ONLY. It will be removed in 1.6. Users should
+			// not attempt to inspect the internal events object using jQuery.data, as this
+			// internal data object is undocumented and subject to change.
+			if ( name === "events" && !thisCache[name] ) {
+				return thisCache[ internalKey ] && thisCache[ internalKey ].events;
+			}
+
+			return getByName ? thisCache[ jQuery.camelCase( name ) ] : thisCache;
+		};
+		})(loadedJQuery);
 		return window.alohaQuery = loadedJQuery;
 	}
 );
