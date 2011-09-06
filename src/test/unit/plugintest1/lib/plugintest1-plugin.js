5b03d2d4cac09cce426db6a28bd0b213d765e0be
draftkraft <h.meran@gentics.com>
2011-09-06 15:09:02 +0200

    Added plugin loading tests

---
+++ src/test/unit/plugintest1/lib/plugintest1-plugin.js
@@ -0,0 +1,34 @@
+/*!
+* Aloha Editor
+* Author & Copyright (c) 2010 Gentics Software GmbH
+* aloha-sales@gentics.com
+* Licensed unter the terms of http://www.aloha-editor.com/license.html
+*/
+define(
+['aloha/core', 'aloha/plugin', 'aloha/jquery', 'i18n!plugintest1/nls/i18n' ],
+function(Aloha ,Plugin, jQuery, i18n ) {
+	"use strict";
+
+     return Plugin.create('plugintest1', {
+		_constructor: function(){
+			this._super('plugintest1');
+		},
+		
+		/**
+		 * Configure the available languages
+		 */
+		languages: ['en', 'de'],
+
+		
+		/**
+		 * Initialize the plugin
+		 */
+		init: function () {
+			var that = this;
+			console.log(Aloha.getPluginUrl('plugintest1'));
+			console.log('plugintest1 initialized');
+
+		}
+		
+	});
+});
