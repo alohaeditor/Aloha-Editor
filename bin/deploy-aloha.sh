a882658adac6317001f9df5083e46440d0831de0
Jenkins Git Master <jenkins@dev6-jenkins.office>
2011-10-06 10:31:25 +0200

    Cleanup of buildscript. Added building of user guides

--- bin/deploy-aloha.sh
+++ bin/deploy-aloha.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $Id: deploy_aloha, v 1.0 2011/10/3 15:50:00 jotschi Exp $
+# $Id: deploy-aloha, v 1.0 2011/10/3 15:50:00 jotschi Exp $
 # 2011 - written by Johannes Schüth <j.schueth@gentics.com>
 
 SCRIPT="`readlink -f $0`" 
