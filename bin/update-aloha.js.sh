214e8be32d8a1b3d390c1cf29e4b58db0aeda9c8
Johannes Schüth <j.schueth@gentics.com>
2011-10-11 13:01:52 +0200

    Updated build script. Now exitcodes are checked as well

--- bin/update-aloha.js.sh
+++ bin/update-aloha.js.sh
@@ -1,8 +1,6 @@
-#!/bin/sh
-
-# Updates the built require-jquery.js file if either jQuery or RequireJS
-# is updated. The new version of either file should be placed in this
-# directory, then this command should be run.
+#!/bin/bash
+# Updates the aloha.js file. This script should be executed when require-jquery.js and/or jQuery and/or RequireJS
+# is updated. The first argument will be the target directory in which the aloha.js will be generated. 
 
 # relative to src/lib/
 REQUIRE='require.js'
@@ -11,36 +9,61 @@ EXT_ADAPT='vendor/ext-3.2.1/adapter/jquery/ext-jquery-adapter-debug.js'
 EXT_ALL='vendor/ext-3.2.1/ext-all-debug.js'
 
 
-## code
+# Generic error check and abort method
+function handleError() {
+  if [ "$1" != "0" ]; then
+     echo -e "\n\nERROR: $2"
+     echo -e "Aborting with errorcode $1 \n\n"
+     exit 10
+  fi
+}
+
+# Simple usage message
+function displayUsage() { 
+        echo "Please add a target directory argument."
+        echo "Example:"
+        echo "        src"
+        echo "Usage:"
+        echo "        $0 [TARGET_DIRECTORY]"
+        exit 1
+}
 
 SCRIPTDIR=`cd \`dirname "$0"\`; pwd`
-TARGET="src"
-TMPDIR="target/tmp/aloha"
-if [ -d "$SCRIPTDIR/../$TMPDIR" ]; then
-	TARGET="$TMPDIR"
+SOURCE="$SCRIPTDIR/../src/lib"
+TARGET="$SCRIPTDIR/../src"
+
+if [ "$1" != "" ]; then
+	TARGET="$SCRIPTDIR/../$1"
+	# Check if the user defined target directory exists
+	if [ ! -d "$TARGET" ]; then
+		echo "Cannot update aloha.js in $TARGET since the directory could not be found."
+		displayUsage
+	fi
 else
-	echo "Cannot update aloha.js in $SCRIPTDIR/../$TMPDIR/lib."
-	exit;
-fi
+        displayUsage
+fi 
 
-LIB="$SCRIPTDIR/../$TARGET/lib"
-echo "Generating aloha.js in $LIB"
-echo > "$LIB/aloha.js"
 
-echo "$REQUIRE"
-cat "$LIB/$REQUIRE" >> "$LIB/aloha.js"
+echo "Generating aloha.js in $TARGET"
+echo > "$TARGET/aloha.js"
 
-echo "$JQUERY"
-cat "$LIB/$JQUERY" >> "$LIB/aloha.js"
+echo "Adding $REQUIRE"
+cat "$SOURCE/$REQUIRE" >> "$TARGET/aloha.js"
+handleError $? "Could not add file to aloha.js"
 
-echo "$EXT_ADAPT"
-echo "$EXT_ALL"
-echo "(function( jQuery ) {" >> "$LIB/aloha.js"
-cat "$LIB/$EXT_ADAPT" "$LIB/$EXT_ALL" >> "$LIB/aloha.js" 
-echo "})( jQuery );" >> "$LIB/aloha.js"
+echo "Adding $JQUERY"
+cat "$SOURCE/$JQUERY" >> "$TARGET/aloha.js"
+handleError $? "Could not add file to aloha.js"
 
-echo "aloha-bootstrap.js"
-cat "$LIB/aloha-bootstrap.js" >> "$LIB/aloha.js"
+echo "Adding $EXT_ADAPT"
+echo "Adding $EXT_ALL"
+echo "(function( jQuery ) {" >> "$TARGET/aloha.js"
+cat "$SOURCE/$EXT_ADAPT" "$SOURCE/$EXT_ALL" >> "$TARGET/aloha.js" 
+handleError $? "Could not add file to aloha.js"
+echo "})( jQuery );" >> "$TARGET/aloha.js"
 
-echo "Updated $LIB/aloha.js."
-echo
+echo "Adding aloha-bootstrap.js"
+cat "$SOURCE/aloha-bootstrap.js" >> "$TARGET/aloha.js"
+handleError $? "Could not add file to aloha.js"
+
+echo "Updated $TARGET/aloha.js."
