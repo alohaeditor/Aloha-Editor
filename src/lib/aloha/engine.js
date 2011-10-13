4518519fd95b34c2e02da98336189f67f4ef2463
Norbert Pomaroli <n.pomaroli@gentics.com>
2011-10-13 17:16:57 +0200

    Fixed calls to fixDisallowedAncestors to add the range object. Fixed adding of end br nodes (they have class aloha-end-br now). Fixed range correction while wrapping nodes (the algorithm of Aryeh Gregor seems to be wrong here)

--- src/lib/aloha/engine.js
+++ src/lib/aloha/engine.js
@@ -1460,23 +1460,30 @@ function wrap(nodeList, siblingCriteria, newParentInstructions, range) {
 		// new parent and offset equal to the index of new parent, add one to
 		// that boundary point's offset."
 		//
-		// Only try to fix the global range.
-		if (globalRange.startContainer == newParent.parentNode
-		&& globalRange.startOffset == getNodeIndex(newParent)) {
-			globalRange.setStart(globalRange.startContainer, globalRange.startOffset + 1);
-		}
-		if (globalRange.endContainer == newParent.parentNode
-		&& globalRange.endOffset == getNodeIndex(newParent)) {
-			globalRange.setEnd(globalRange.endContainer, globalRange.endOffset + 1);
-		}
 		// Try to fix range
-		if (range.startContainer == newParent.parentNode
-		&& range.startOffset == getNodeIndex(newParent)) {
-			range.setStart(range.startContainer, range.startOffset + 1);
+		var startContainer = range.startContainer, startOffset = range.startOffset,
+			endContainer = range.endContainer, endOffset = range.endOffset;
+		if (startContainer == newParent.parentNode
+		&& startOffset >= getNodeIndex(newParent)) {
+			range.setStart(startContainer, startOffset + 1);
 		}
-		if (range.endContainer == newParent.parentNode
-		&& range.endOffset == getNodeIndex(newParent)) {
-			range.setEnd(range.endContainer, range.endOffset + 1);
+		if (endContainer == newParent.parentNode
+		&& endOffset >= getNodeIndex(newParent)) {
+			range.setEnd(endContainer, endOffset + 1);
+		}
+
+		// Only try to fix the global range. TODO remove globalRange here
+		if (globalRange && globalRange !== range) {
+			startContainer = globalRange.startContainer, startOffset = globalRange.startOffset,
+				endContainer = globalRange.endContainer, endOffset = globalRange.endOffset;
+			if (startContainer == newParent.parentNode
+			&& startOffset >= getNodeIndex(newParent)) {
+				globalRange.setStart(startContainer, startOffset + 1);
+			}
+			if (endContainer == newParent.parentNode
+			&& endOffset >= getNodeIndex(newParent)) {
+				globalRange.setEnd(endContainer, endOffset + 1);
+			}
 		}
 	}
 
@@ -4661,7 +4668,7 @@ function deleteContents() {
 		if ((isEditable(parent_) || isEditingHost(parent_))
 		&& !isInlineNode(parent_)
 		&& !parent_.hasChildNodes()) {
-			parent_.appendChild(document.createElement("br"));
+			parent_.appendChild(createEndBreak());
 		}
 	}
 
@@ -4899,7 +4906,7 @@ function deleteContents() {
 	// "If start block has no children, call createElement("br") on the context
 	// object and append the result as the last child of start block."
 	if (!startBlock.hasChildNodes()) {
-		startBlock.appendChild(document.createElement("br"));
+		startBlock.appendChild(createEndBreak());
 	}
 
 	// "Restore states and values from overrides."
@@ -5319,7 +5326,7 @@ function indentNodes(nodeList, range) {
 	);
 
 	// "Fix disallowed ancestors of new parent."
-	fixDisallowedAncestors(newParent);
+	fixDisallowedAncestors(newParent, range);
 }
 
 function outdentNode(node, range) {
@@ -5429,7 +5436,7 @@ function outdentNode(node, range) {
 
 		// "Fix disallowed ancestors of each member of children."
 		for (var i = 0; i < children.length; i++) {
-			fixDisallowedAncestors(children[i]);
+			fixDisallowedAncestors(children[i], range);
 		}
 
 		// "Abort these steps."
@@ -5637,7 +5644,7 @@ function toggleLists(tagName, range) {
 
 			// "Fix disallowed ancestors of each member of sublist."
 			for (var i = 0; i < sublist.length; i++) {
-				fixDisallowedAncestors(sublist[i]);
+				fixDisallowedAncestors(sublist[i], range);
 			}
 
 			// "Restore the values from values."
@@ -5777,6 +5784,7 @@ function toggleLists(tagName, range) {
 					},
 					range
 				)
+				, range
 			);
 		}
 	}
@@ -5898,6 +5906,16 @@ function justifySelection(alignment, range) {
 			range
 		);
 	}
+}
+
+
+//@}
+///// Create an end break /////
+//@{
+function createEndBreak() {
+	var endBr = document.createElement("br");
+	endBr.setAttribute("class", "aloha-end-br");
+	return endBr;
 }
 
 
@@ -6370,7 +6388,8 @@ commands.formatblock = {
 						: function() { return false },
 					function() { return document.createElement(value) },
 					range
-				)
+				),
+				range
 			);
 		}
 	}, indeterm: function() {
@@ -6808,7 +6827,7 @@ commands.inserthorizontalrule = {
 		range.insertNode(hr);
 
 		// "Fix disallowed ancestors of hr."
-		fixDisallowedAncestors(hr);
+		fixDisallowedAncestors(hr, range);
 
 		// "Run collapse() on the Selection, with first argument equal to the
 		// parent of hr and the second argument equal to one plus the index of
@@ -6881,7 +6900,7 @@ commands.inserthtml = {
 		// the result as the last child of the active range's start node."
 		if (isBlockNode(range.startContainer)
 		&& ![].some.call(range.startContainer.childNodes, isVisible)) {
-			range.startContainer.appendChild(document.createElement("br"));
+			range.startContainer.appendChild(createEndBreak());
 		}
 
 		// "Call collapse() on the context object's Selection, with last
@@ -7035,9 +7054,7 @@ commands.insertlinebreak = {
 		// context object and let extra br be the result, then call
 		// insertNode(extra br) on the active range."
 		if (isCollapsedLineBreak(br)) {
-			var endBr = document.createElement("br");
-			endBr.setAttribute("class", "aloha-end-br");
-			range.insertNode(endBr);
+			range.insertNode(createEndBreak());
 
 			// Compensate for nonstandard implementations of insertNode
 			Aloha.getSelection().collapse(br.parentNode, 1 + getNodeIndex(br));
@@ -7157,7 +7174,7 @@ commands.insertparagraph = {
 
 				// "Call createElement("br") on the context object, and append
 				// the result as the last child of container."
-				container.appendChild(document.createElement("br"));
+				container.appendChild(createEndBreak());
 
 				// "Call collapse(container, 0) on the context object's
 				// Selection."
@@ -7212,7 +7229,7 @@ commands.insertparagraph = {
 			// Work around browser bugs: some browsers select the
 			// newly-inserted node, not per spec.
 			if (!isDescendant(nextNode(br), container)) {
-				range.insertNode(document.createElement("br"));
+				range.insertNode(createEndBreak());
 				Aloha.getSelection().collapse(node, offset + 1);
 				range.setEnd(node, offset + 1);
 			}
@@ -7234,7 +7251,7 @@ commands.insertparagraph = {
 			// context object and append the result as the last child of
 			// container."
 			if (!container.hasChildNodes()) {
-				container.appendChild(document.createElement("br"));
+				container.appendChild(createEndBreak());
 			}
 
 			// "If container is a dd or dt, and it is not an allowed child of
@@ -7250,7 +7267,7 @@ commands.insertparagraph = {
 			}
 
 			// "Fix disallowed ancestors of container."
-			fixDisallowedAncestors(container);
+			fixDisallowedAncestors(container, range);
 
 			// "Abort these steps."
 			return;
@@ -7351,14 +7368,14 @@ commands.insertparagraph = {
 		// the context object, and append the result as the last child of
 		// container."
 		if (![].some.call(container.childNodes, isVisible)) {
-			container.appendChild(document.createElement("br"));
+			container.appendChild(createEndBreak());
 		}
 
 		// "If new container has no visible children, call createElement("br")
 		// on the context object, and append the result as the last child of
 		// new container."
 		if (![].some.call(newContainer.childNodes, isVisible)) {
-			newContainer.appendChild(document.createElement("br"));
+			newContainer.appendChild(createEndBreak());
 		}
 
 		// "Call collapse(new container, 0) on the context object's Selection."
