332676346a91240781385b4f41da0604134f8f19
nka11 <nka11@nka.me>
2011-10-21 22:12:28 +0200

    Cleaner sidebar/css - elements remains unlinked

--- src/plugins/common/image/lib/image-sidebar.js
+++ src/plugins/common/image/lib/image-sidebar.js
@@ -15,52 +15,55 @@ function(aQuery, i18n, i18nCore){
 			pl.sidebar = sidebar;
 			this.pl = pl;
 			sidebar.addPanel({
+				id       : sb.nsClass('sidebar-panel-target'),
+				title    : i18n.t('floatingmenu.tab.img'),
+				content  : '',
+				expanded : true,
+				activeOn : 'img, image',
+				
+				onInit     : function () {
+					 var that = this,
+						 content = this.setContent(
+							'<div class="' + sb.nsClass('src-container') + '"><fieldset><legend>' + i18n.t('field.img.src.label') + '</legend>' +
+							'<input type="text" class="' + sb.nsClass('imageSrc') + '" /></fieldset></div>' + 
+							'<div class="' + sb.nsClass('title-container') + '" ><fieldset><legend>' + i18n.t('field.img.title.label') +
+							'</legend><input type="text" class="' + sb.nsClass('imageTitle') + '" /></fieldset></div>').content; 
+					 
+					 jQuery( sb.nsSel('framename') ).live( 'keyup', function() {
+						jQuery( that.effective )
+							.attr( "src", jQuery(this).val().replace("\"", '&quot;').replace("'", "&#39;") );
+					 });
+					 jQuery( sb.nsSel('imageTitle') ).live( 'keyup', function() {
+						jQuery( that.effective )
+							.attr( "title", jQuery(this).val().replace("\"", '&quot;').replace("'", "&#39;") );
+					 });
+				},
+				
+				onActivate: function (effective) {
+					var that = this;
+					that.effective = effective;
 					
-					id       : sb.nsClass('sidebar-panel-target'),
-					title    : i18n.t('floatingmenu.tab.img'),
-					content  : '',
-					expanded : true,
-					activeOn : 'img, image',
-					
-					onInit     : function () {
-						 var that = this,
-							 content = this.setContent(
-								'<div class="' + sb.nsClass('src-container') + '"><fieldset><legend>' + i18n.t('field.img.src.label') + '</legend>' +
-								'</fieldset></div>' + 
-								'<div class="' + sb.nsClass('title-container') + '" ><fieldset><legend>' + i18n.t('field.img.title.label') + '</legend><input type="text" class="' + sb.nsClass('imageTitle') + '" /></fieldset></div>').content; 
-						 
-						 jQuery( sb.nsSel('framename') ).live( 'keyup', function() {
-							jQuery( that.effective )
-								.attr( "src", jQuery(this).val().replace("\"", '&quot;').replace("'", "&#39;") );
-						 });
-						 jQuery( sb.nsSel('imageTitle') ).live( 'keyup', function() {
-							jQuery( that.effective )
-								.attr( "title", jQuery(this).val().replace("\"", '&quot;').replace("'", "&#39;") );
-						 });
-					},
-					
-					onActivate: function (effective) {
-						var that = this;
-						that.effective = effective;
-						
-						var that = this;
-						that.effective = effective;
-						jQuery( sb.nsSel('imageTitle') ).val( jQuery(that.effective).attr('title') );
-					}
-					
-				});
+					var that = this;
+					that.effective = effective;
+					jQuery( sb.nsSel('imageTitle') ).val( jQuery(that.effective).attr('title') );
+				}
+				
+			});
+			
 			sidebar.show();
 		},
 		nsSel: function () {
-			var stringBuilder = [], prefix = imageNamespace;
-			jQuery.each(arguments, function () { stringBuilder.push('.' + (this.pl == '' ? prefix : prefix + '-' + this.pl)); });
+			var stringBuilder = [], prefix = imageNamespace,
+				that = this;
+			jQuery.each(arguments, function () { stringBuilder.push('.' + (this == '' ? prefix : prefix + '-' + this)); });
 			return stringBuilder.join(' ').trim();
 		},
 
 		//Creates string with this component's namepsace prefixed the each classname
 		nsClass: function () {
-			var stringBuilder = [], prefix = imageNamespace;
-			jQuery.each(arguments, function () { stringBuilder.push(this.pl == '' ? prefix : prefix + '-' + this.pl); });
+			var stringBuilder = [], prefix = imageNamespace,
+				that = this;
+			jQuery.each(arguments, function () { stringBuilder.push(this == '' ? prefix : prefix + '-' + this); });
 			return stringBuilder.join(' ').trim();
 		},
 		update: function() {
