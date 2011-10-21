ae4a03d67e7f55578fce59072e16349e4308b511
Petro Salema <p.salema@gentics.com>
2011-10-21 18:35:52 +0200

    Refactoring table tests

--- src/test/unit/tabletests.js
+++ src/test/unit/tabletests.js
@@ -18,71 +18,61 @@ function( TestUtils ) {
 	
 	var tests = [
 		
-		//
-		//	  NB:
-		//	------------------------------------------------------------------
-		//	  The table plugin currently does not disactivate correctly when
-		//	  mahalo is called, so are unable to run this test together. we
-		//	  can only run one at a time
-		//	------------------------------------------------------------------
-		//
-		
-		//
-		// Activation/deactivation
-		//
+		{ module : 'Activation/deactivation' },
 		
 		{
+			desc	  : 'Activate and deactivate a table',
 			start     : '<table><tbody><tr><td>foo</td></tr></tbody></table>',
 			expected  : '<table><tbody><tr><td>foo</td></tr></tbody></table>',
 			operation : function ( table ) {}
 		},
 		
 		//
-		// Column selection
+		//	  NB:
+		//	------------------------------------------------------------------
+		//	  selectRow and selectColumns has a bug where it allows us to
+		//	  programmatically select the helper column (and row).
+		//	  All following tests will work around this fault by using
+		//	  1-indexing with selectcolumns rather than 0 based indexing
+		//	------------------------------------------------------------------
 		//
 		
+		{ module : 'Row/column selection' },
+		
 		{
+			desc	  : 'Select column by index',
 			start     : '<table><tbody><tr><td>foo</td></tr></tbody></table>',
-			expected  : '<table><tbody><tr><td>foo</td><td>&nbsp;</td></tr></tbody></table>',
+			expected  : '<table><tbody><tr><td>bar</td></tr></tbody></table>',
 			operation : function ( table ) {
-				table.selectColumns( [ 0 ] ); 
-				table.addColumnsRight();
+				table.selectColumns( [ 0 ] );
+				table.obj.find( '.aloha-cell-selected' )
+					 .html( 'bar' )
+					 .removeAttr( 'class' )
+					 .removeAttr( 'style' );
 			}
 		},
 		
-		//
-		//	  NB:
-		//	------------------------------------------------------------------
-		//	  Selectcolumns has a bug where it allows us to programmatically
-		//	  select the helper column (and row).
-		//	  All following tests will work around this fault by using
-		//	  1-indexing with selectcolumns
-		//	------------------------------------------------------------------
-		//
+		{
+			desc	  : 'Select row by index',
+			start     : '<table><tbody><tr><td>foo</td></tr></tbody></table>',
+			expected  : '<table><tbody><tr><td>bar</td></tr></tbody></table>',
+			operation : function ( table ) {
+				table.selectRows( [ 0 ] );
+				table.obj.find( '.aloha-cell-selected' )
+					 .html( 'bar' )
+					 .removeAttr( 'class' )
+					 .removeAttr( 'style' );
+			}
+		},
+		
+		{ module : 'Inserting/removing columns' },
 		
 		//
 		// Inserting/removing cols
 		//
 		
 		{
-			start     : '<table><tbody><tr><td>foo</td></tr></tbody></table>',
-			expected  : '<table><tbody><tr><td>foo</td><td>&nbsp;</td></tr></tbody></table>',
-			operation : function ( table ) {
-				table.selectColumns( [ 1 ] ); 
-				table.addColumnsRight();
-			}
-		},
-		
-		{
-			start     : '<table><tbody><tr><td>foo</td></tr></tbody></table>',
-			expected  : '<table><tbody><tr><td>&nbsp;</td><td>foo</td></tr></tbody></table>',
-			operation : function ( table ) {
-				table.selectColumns( [ 0 ] ); 
-				table.addColumnsLeft();
-			}
-		},
-		
-		{
+			desc	  : 'Insert column right of column at index 0',
 			start     : '<table><tbody><tr><td>foo</td><td>bar</td></tr></tbody></table>',
 			expected  : '<table><tbody><tr><td>foo</td><td>&nbsp;</td><td>bar</td></tr></tbody></table>',
 			operation : function ( table ) {
@@ -92,31 +82,49 @@ function( TestUtils ) {
 		},
 		
 		{
-			start     : '<table><tbody><tr><td>foo</td></tr><tr><td>bar</td></tr></tbody></table>',
-			expected  : '<table><tbody><tr><td>&nbsp;</td><td>foo</td></tr><tr><td>&nbsp;</td><td>bar</td></tr></tbody></table>',
+			desc	  : 'Insert column right of column at index 1',
+			start     : '<table><tbody><tr><td>foo</td></tr></tbody></table>',
+			expected  : '<table><tbody><tr><td>foo</td><td>&nbsp;</td></tr></tbody></table>',
 			operation : function ( table ) {
-				table.selectColumns( [ 0 ] ); 
+				table.selectColumns( [ 1 ] ); 
 				table.addColumnsRight();
 			}
 		},
 		
 		{
-			start     : '<table><tbody><tr><td>foo</td><td>bar</td></tr><tr><td>foo1</td><td>bar2</td></tr></tbody></table>',
-			expected  : '<table><tbody><tr><td>foo</td><td>&nbsp;</td><td>bar</td></tr><tr><td>foo1</td><td>&nbsp;</td><td>bar2</td></tr></tbody></table>',
+			desc	  : 'Insert column left of 1st column',
+			start     : '<table><tbody><tr><td>foo</td></tr></tbody></table>',
+			expected  : '<table><tbody><tr><td>&nbsp;</td><td>foo</td></tr></tbody></table>',
+			operation : function ( table ) {
+				table.selectColumns( [ 0 ] ); 
+				table.addColumnsLeft();
+			}
+		},
+		
+		{
+			desc	  : 'Add column left of 2nd column',
+			start     : '<table><tbody>\
+							<tr><td>foo</td><td>bar</td></tr>\
+							<tr><td>foo1</td><td>bar2</td></tr>\
+						 </tbody></table>',
+			expected  : '<table><tbody>\
+							<tr><td>foo</td><td>&nbsp;</td><td>bar</td></tr>\
+							<tr><td>foo1</td><td>&nbsp;</td><td>bar2</td></tr>\
+						 </tbody></table>',
 			operation : function ( table ) {
 				table.selectColumns( [ 2 ] ); 
 				table.addColumnsLeft();
 			}
 		},
 		
+		{ module : 'Inserting/removing rows' },
+		
 		//
 		// Inserting/removing rows
 		//
 		
-		// In this case  we are only interested that a column is inserted we
-		// ignore the fact that there is an error with addColumnsRight where it
-		// the 0 index is the selection helper columns
 		{
+			desc	  : 'Add row at index 0',
 			start     : '<table><tbody><tr><td>foo</td></tr></tbody></table>',
 			expected  : '<table><tbody><tr><td>foo</td></tr><tr><td>&nbsp;</td></tr></tbody></table>',
 			operation : function ( table ) {
@@ -125,6 +133,7 @@ function( TestUtils ) {
 		},
 		
 		{
+			desc	  : 'Add row at index 1',
 			start     : '<table><tbody><tr><td>foo</td></tr><tr><td>bar</td></tr></tbody></table>',
 			expected  : '<table><tbody><tr><td>foo</td></tr><tr><td>&nbsp;</td></tr><tr><td>bar</td></tr></tbody></table>',
 			operation : function ( table ) {
@@ -132,11 +141,10 @@ function( TestUtils ) {
 			}
 		},
 		
-		//
-		// Merging
-		//
+		{ module : 'Merging cells' },
 		
 		{
+			desc	  : 'Basic columns merging',
 			start     : '<table><tbody>\
 							<tr><td class="aloha-cell-selected">foo</td></tr>\
 							<tr><td class="aloha-cell-selected">bar</td></tr>\
@@ -152,6 +160,7 @@ function( TestUtils ) {
 		},
 		
 		{
+			desc	  : 'Merging columns',
 			start     : '<table><tbody>\
 							<tr><td class="aloha-cell-selected"><i>foo</i></td></tr>\
 							<tr><td class="aloha-cell-selected">bar</td></tr>\
@@ -299,27 +308,39 @@ function( TestUtils ) {
 		
 		for ( var i = 0; i < tests.length; i++ ) {
 			testcase = tests[ i ];
+			
+			if ( testcase.module ) {
+				module( testcase.module.toUpperCase() + ' :' );
+				continue;
+			}
+			
 			start = style_html( testcase.start );
 			expected = style_html( testcase.expected );
 			
-			// Place test contents into our editable
+			// Place test contents into our editable, and activate the editable
 			editable.html( start ).aloha();
 			
 			if ( typeof testcase.operation === 'function' ) {
-				// Click the editable to trigger the aloha-editable-activated 
-				// event Then click on the table to activate
-				editable.mousedown().mouseup().find( 'table' ).mousedown();
+				editable
+					.mousedown() // tigger the aloha-editable-activated event
+					.mouseup()	 // this is needed to get the table plugin to deactivate correctly
+						.find( 'table' )
+							.mousedown();
 				testcase.operation( TablePlugin.activeTable );
 			}
 			
 			editable.mahalo();
 			
-			test( 'table test', { start: start, expected: expected }, function() {
-				var result = editable.html().toLowerCase();
-				result = result.replace( /(<table.*?)\s*id\s*=\s*[\"\'][^\"\']*[\"\']/ig, '$1' );
-				result = style_html( result );
-				deepEqual( result, expected, 'Check Operation Result' );
-			});
+			test(
+				( testcase.desc || 'Test' ).toUpperCase(),
+				{ start: start, expected: expected },
+				function() {
+					var result = editable.html().toLowerCase();
+					result = result.replace( /(<table.*?)\s*id\s*=\s*[\"\'][^\"\']*[\"\']/ig, '$1' );
+					result = style_html( result );
+					deepEqual( result, expected, 'Check Operation Result' );
+				}
+			);
 		}
 	});
 });
