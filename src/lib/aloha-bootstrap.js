50491f6dc0ab86741b7c5ba56f0fb120443f533e
Petro Salema <p.salema@gentics.com>
2011-10-24 12:06:56 -0700

    Refactors aloha-bootstrap.js

--- src/lib/aloha-bootstrap.js
+++ src/lib/aloha-bootstrap.js
@@ -23,23 +23,23 @@
 	// load jQuery with noConflict (true) to remove anyway from global namespace
 	// a user may add it by its own with var jQuery = $ = Aloha.jQuery;
 	var 
-		jQuery = window.jQuery.noConflict( true ),
-		deferredReady,
-		alohaRequire;
+	    jQuery = window.jQuery.noConflict( true ),
+	    deferredReady,
+	    alohaRequire;
 	
 	// Ensure Aloha settings namespace and default
 	window.Aloha = window.Aloha || {};
-
+	
 	// reset defaults. Users should use settings. 
 	Aloha.defaults = {};
-
+	
 	// guarantee the settings namespace even if not set by user
 	Aloha.settings = Aloha.settings || {};
-
+	
 	// set jQuery to buildin of not otherwise set
 	// From here on Aloha.jQuery is always available
 	Aloha.jQuery = Aloha.settings.jQuery || Aloha.jQuery || jQuery || null;
-
+	
 	// Aloha define, require, preserve original require
 	Aloha._require = require;
 	Aloha.define = define;
@@ -52,29 +52,29 @@
 	// no jQuery at this stage...
 	function getBaseUrl () {
 		var
-			baseUrl = './',
-			script,
-			scripts = document.getElementsByTagName( 'script' ),
-			i, j = scripts.length,
-			regexAlohaJs = /\/aloha.js$/,
-			regexJs = /[^\/]*\.js$/;
+		    baseUrl = './',
+		    script,
+		    scripts = document.getElementsByTagName( 'script' ),
+		    i, j = scripts.length,
+		    regexAlohaJs = /\/aloha.js$/,
+		    regexJs = /[^\/]*\.js$/;
 		
-        for ( i = 0; i < j && ( script = scripts[ i ] ); ++i ) {
-            // take aloha.js or first ocurrency of data-aloha-plugins 
-        	// and script ends with .js
-        	if ( regexAlohaJs.test( script.src ) ) {
-        		baseUrl = script.src.replace( regexAlohaJs , '' );
-        		break;
-        	}            
-            if ( baseUrl === './' && script.getAttribute( 'data-aloha-plugins')
-            		&& regexJs.test(script.src ) ) {
-            	baseUrl = script.src.replace( regexJs , '' );
-            }
-        }
-        
+		for ( i = 0; i < j && ( script = scripts[ i ] ); ++i ) {
+			// take aloha.js or first ocurrency of data-aloha-plugins 
+			// and script ends with .js
+			if ( regexAlohaJs.test( script.src ) ) {
+				baseUrl = script.src.replace( regexAlohaJs , '' );
+				break;
+			}            
+			if ( baseUrl === './' && script.getAttribute( 'data-aloha-plugins' )
+				&& regexJs.test(script.src ) ) {
+			baseUrl = script.src.replace( regexJs , '' );
+			}
+		}
+        	
 		return baseUrl;
 	};
-
+	
 	// prepare the require config object and remember it
 	Aloha.settings.requireConfig = {
 		context: 'aloha',
@@ -93,33 +93,34 @@
 	};
 	
 	// create promise for 'aloha-ready' when Aloha is not yet ready
-    // and fire later when 'aloha-ready' is triggered all other events bind
+	// and fire later when 'aloha-ready' is triggered all other events bind
 	deferredReady = Aloha.jQuery.Deferred();
-    Aloha.bind = function ( type, fn ) {
-    	if ( type == 'aloha-ready' ) {
-    		if ( Aloha.stage != 'alohaReady' ) {
-    			deferredReady.done( fn );
-    		} else {
-    			fn();
-    		}
-    	} else {
-    		Aloha.jQuery( Aloha, 'body' ).bind( type, fn );
-    	}
-    	return this;
-    };
+	Aloha.bind = function ( type, fn ) {
+	    	if ( type == 'aloha-ready' ) {
+			if ( Aloha.stage != 'alohaReady' ) {
+				deferredReady.done( fn );
+			} else {
+				fn();
+			}
+		} else {
+			Aloha.jQuery( Aloha, 'body' ).bind( type, fn );
+		}
 	
-    Aloha.trigger = function ( type, data ) {
-    	if ( type == 'aloha-ready' ) {
-    		// resolve all deferred events on dom ready and delete local var
-    		Aloha.jQuery( deferredReady.resolve );
-    	}
-    	Aloha.jQuery( Aloha, 'body' ).trigger( type, data );
-    	return this;
-    };
-    
+		return this;
+	};
+	
+	Aloha.trigger = function ( type, data ) {
+		if ( type == 'aloha-ready' ) {
+			// resolve all deferred events on dom ready and delete local var
+			Aloha.jQuery( deferredReady.resolve );
+		}
+		Aloha.jQuery( Aloha, 'body' ).trigger( type, data );
+		return this;
+	};
+    	
 	Aloha.ready = function ( fn ) {
-    	this.bind( 'aloha-ready', fn );
-    	return this;
+		this.bind( 'aloha-ready', fn );
+		return this;
 	};
 	
 	// requireJs error handling
