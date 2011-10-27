6b8bdbd1fb8c17c3125ba1f8f41e32f4700ac567
Petro Salema <p.salema@gentics.com>
2011-10-27 16:34:20 +0200

    Fixes when smartContentChanged is called to ensure that it is invoked after pasted content is within the editable

--- src/lib/aloha/editable.js
+++ src/lib/aloha/editable.js
@@ -721,58 +721,68 @@ function(Aloha, Class, jQuery, PluginManager, FloatingMenu, Selection, Markup, C
 		},
 
 		/**
-		 * Handle a smartContentChange; This is used for smart actions within the content/while editing.
+		 * Generates and signals a smartContentChange event.
+		 *
+		 * A smart content change occurs when a special editing action, or a
+		 * combination of interactions are performed by the user during the
+		 * course of editing within an editable.
+		 * The smart content change event would therefore signal to any
+		 * component that is listening to this event, that content has been
+		 * inserted into the editable that may need to be prococessed in a
+		 * special way 
+		 * This is used for smart actions within the content/while editing.
 		 * @hide
 		 */
-		smartContentChange: function(event) {
+		smartContentChange: function( event ) {
 			var me = this,
 				uniChar = null,
 				re, match;
 
 			// ignore meta keys like crtl+v or crtl+l and so on
-			if (event && (event.metaKey || event.crtlKey || event.altKey)) {
+			if ( event && ( event.metaKey || event.crtlKey || event.altKey ) ) {
 				return false;
 			}
 
-			if (event && event.originalEvent) {
-
+			if ( event && event.originalEvent ) {
 				// regex to strip unicode
-				re = new RegExp("U\\+(\\w{4})");
-				match = re.exec(event.originalEvent.keyIdentifier);
+				re = new RegExp( "U\\+(\\w{4})" );
+				match = re.exec( event.originalEvent.keyIdentifier );
 
 				// Use keyIdentifier if available
-				if ( event.originalEvent.keyIdentifier && 1 == 2) {
-					if (match !== null) {
-						uniChar = unescape('%u' + match[1]);
+				if ( event.originalEvent.keyIdentifier && 1 == 2 ) {
+					if ( match !== null ) {
+						uniChar = unescape( '%u' + match[ 1 ] );
 					}
-					if (uniChar === null) {
+					if ( uniChar === null ) {
 						uniChar = event.originalEvent.keyIdentifier;
 					}
 				// FF & Opera don't support keyIdentifier
 				} else {
 					// Use among browsers reliable which http://api.jquery.com/keypress
-					uniChar = (this.keyCodeMap[this.keyCode] || String.fromCharCode(event.which) || 'unknown');
+					uniChar = ( this.keyCodeMap[ this.keyCode ] ||
+									String.fromCharCode( event.which ) || 'unknown' );
 				}
 			}
+			
 			// handle "Enter" -- it's not "U+1234" -- when returned via "event.originalEvent.keyIdentifier"
 			// reference: http://www.w3.org/TR/2007/WD-DOM-Level-3-Events-20071221/keyset.html
-			if (jQuery.inArray(uniChar, this.sccDelimiters) >= 0) {
+			if ( jQuery.inArray( uniChar, this.sccDelimiters ) >= 0 ) {
 
-				clearTimeout(this.sccTimerIdle);
-				clearTimeout(this.sccTimerDelay);
+				clearTimeout( this.sccTimerIdle );
+				clearTimeout( this.sccTimerDelay );
 
-				this.sccTimerDelay = setTimeout(function() {
+				this.sccTimerDelay = setTimeout( function() {
 
-					Aloha.trigger('aloha-smart-content-changed',{
+					Aloha.trigger( 'aloha-smart-content-changed', {
 						'editable' : me,
 						'keyIdentifier' : event.originalEvent.keyIdentifier,
 						'keyCode' : event.keyCode,
 						'char' : uniChar,
 						'triggerType' : 'keypress', // keypress, timer, blur, paste
 						'snapshotContent' : me.getSnapshotContent()
-					});
+					} );
 
-					Aloha.Log.debug(this, 'smartContentChanged: event type keypress triggered');
+					Aloha.Log.debug( this, 'smartContentChanged: event type keypress triggered' );
 	/*
 					var r = Aloha.Selection.rangeObject;
 					if (r.isCollapsed()
@@ -801,51 +811,45 @@ function(Aloha, Class, jQuery, PluginManager, FloatingMenu, Selection, Markup, C
 
 					}
 	*/
-				},this.sccDelay);
+				}, this.sccDelay );
 			}
 
-			else if (uniChar !== null) {
-
-				this.sccTimerIdle = setTimeout(function() {
-
-					// in the rare case idle time is lower then delay time
-					clearTimeout(this.sccTimerDelay);
-
-					Aloha.trigger('aloha-smart-content-changed',{
-						'editable' : me,
-						'keyIdentifier' : null,
-						'keyCode' : null,
-						'char' : null,
-						'triggerType' : 'idle',
-						'snapshotContent' : me.getSnapshotContent()
-					});
-
-				},this.sccIdle);
-
-			}
-
-			else if (event && event.type === 'paste') {
-				Aloha.trigger('aloha-smart-content-changed',{
+			else if ( event && event.type === 'paste' ) {
+				Aloha.trigger( 'aloha-smart-content-changed', {
 					'editable' : me,
 					'keyIdentifier' : null,
 					'keyCode' : null,
 					'char' : null,
 					'triggerType' : 'paste', // paste
 					'snapshotContent' : me.getSnapshotContent()
-				});
-
+				} );
 			}
 
-			else if (event && event.type === 'blur') {
-				Aloha.trigger('aloha-smart-content-changed',{
+			else if ( event && event.type === 'blur' ) {
+				Aloha.trigger( 'aloha-smart-content-changed',{
 					'editable' : me,
 					'keyIdentifier' : null,
 					'keyCode' : null,
 					'char' : null,
 					'triggerType' : 'blur',
 					'snapshotContent' : me.getSnapshotContent()
-				});
+				} );
+			}
 
+			else if ( uniChar !== null ) {
+				this.sccTimerIdle = setTimeout( function() {
+					// in the rare case idle time is lower then delay time
+					clearTimeout( this.sccTimerDelay );
+
+					Aloha.trigger( 'aloha-smart-content-changed', {
+						'editable' : me,
+						'keyIdentifier' : null,
+						'keyCode' : null,
+						'char' : null,
+						'triggerType' : 'idle',
+						'snapshotContent' : me.getSnapshotContent()
+					} );
+				}, this.sccIdle );
 			}
 
 		},
