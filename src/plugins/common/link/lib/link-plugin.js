7fc0b94b492041b065b91b17f21b51f3d6f89ff7
Johannes Schüth <j.schueth@gentics.com>
2011-11-09 18:00:05 +0100

    Fixed handling of external links when editing a link which contains a repository link

--- src/plugins/common/link/lib/link-plugin.js
+++ src/plugins/common/link/lib/link-plugin.js
@@ -185,7 +185,7 @@ define( [
 								jQuery( pl.nsSel('framename') ).val( jQuery(that.effective).attr('target') );
 								jQuery( pl.nsSel('framename') ).show();
 							}
-						}else {
+						} else {
 							jQuery( pl.nsSel('radioTarget') ).first().attr('checked', 'checked');
 							jQuery( that.effective ).attr( 'target', jQuery(pl.nsSel('radioTarget')).first().val() );
 						}
@@ -361,15 +361,16 @@ define( [
 
 				that.hrefChange();
 				
-				
 				// Handle the enter key. Terminate the link scope and show the final link.
 				if (event.keyCode == 13) {
+										
 					// Update the selection and place the cursor at the end of the link.
 					var	range = Aloha.Selection.getRangeObject();
 					
 					// workaround to keep the found markup otherwise removelink won't work
-					var foundMarkup = that.findLinkMarkup( range );
-					that.hrefField.setTargetObject(foundMarkup, 'href');
+//					var foundMarkup = that.findLinkMarkup( range );
+//					console.dir(foundMarkup);
+//					that.hrefField.setTargetObject(foundMarkup, 'href');
 					
 					that.ignoreNextSelectionChangedEvent = true;
 					range.startContainer = range.endContainer;
@@ -378,7 +379,8 @@ define( [
 					//that.ignoreNextSelectionChangedEvent = true;
 					
 					var hrefValue = jQuery(that.hrefField.extButton.el.dom).attr('value');
-					if (hrefValue ==="http://" || hrefValue === "") {
+					
+					if (hrefValue === "http://" || hrefValue === "") {
 						that.removeLink(false);
 					}
 					
