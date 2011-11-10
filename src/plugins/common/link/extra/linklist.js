68d6d5d573b7153257f3c10f9a60d4a0e6a3089d
Petro Salema <p.salema@gentics.com>
2011-11-10 14:08:44 +0100

    Refactors linklist.js

--- src/plugins/common/link/extra/linklist.js
+++ src/plugins/common/link/extra/linklist.js
@@ -5,27 +5,28 @@
 * Licensed unter the terms of http://www.aloha-editor.com/license.html
 */
 
-define(['aloha/jquery', 'aloha/repository', 'i18n!aloha/nls/i18n'],
-function(jQuery, repository, i18nCore) {
-	"use strict";
+/**
+ * Aloha Link List Repository
+ * --------------------------
+ * A simple demo repository of links
+ */
+
+define( [ 'aloha/jquery', 'aloha/repository', 'i18n!aloha/nls/i18n' ],
+function ( jQuery, repository, i18nCore ) {
+	'use strict';
 	
-	var 
-		GENTICS = window.GENTICS;
-	
-	/**
-	 * Create the repository
-	 */
-	var linkListRepository = new (repository.extend({
-		_constructor: function(){
-			this._super('linklist');
+	var linkListRepository = new ( repository.extend( {
+		
+		_constructor: function (){
+			this._super( 'linklist' );
 		},
-	
+		
 		/**
 		 * configure data as array with following format:
 		 *
 		 * [
-		 * { name: 'Aloha Editor - The HTML5 Editor', url:'http://aloha-editor.com', type:'website' },
-		 * { name: 'Aloha Logo', url:'http://www.aloha-editor.com/images/aloha-editor-logo.png', type:'image'  }
+		 *   { name: 'Aloha Editor - The HTML5 Editor', url:'http://aloha-editor.com', type:'website' },
+		 *   { name: 'Aloha Logo', url:'http://www.aloha-editor.com/images/aloha-editor-logo.png', type:'image'  }
 		 * ];
 		 *
 		 * @property
@@ -39,7 +40,7 @@ function(jQuery, repository, i18nCore) {
 		],
 		
 		/**
-		 * Internal folder structur.
+		 * Internal folder structure
 		 * @hide
 		 */
 		folder: [],
@@ -48,55 +49,51 @@ function(jQuery, repository, i18nCore) {
 		 * initalize LinkList, parse all links, build folder structure and add
 		 * additional properties to the items
 		 */
-		init: function() {
-		
-			// Prepare
-			var me = this;
+		init: function () {
+			var i = 0,
+				l = this.urlset.length,
+				j;
 			
 			// generate folder structure
-		    for (var i = 0; i < this.urlset.length; i++) {
-		
-		    	var e = this.urlset[i];
+		    for ( ; i < l; i++ ) {
+		    	var e = this.urlset[ i ];
 		    	e.repositoryId = this.repositoryId;
 		    	e.id = e.id ? e.id : e.url;
-		    	var u = e.uri = this.parseUri(e.url);
-		
-		    	// add hostname as root folder
-		    	var path = this.addFolder('', u.host);
-		
-		    	var pathparts = u.path.split('/');
-		    	for (var j = 0; j < pathparts.length; j++) {
-		    		if (
-		    			pathparts[j] &&
-		    			// It's a file because it has an extension.
-		    			// Could improve this one :)
-		    			pathparts[j].lastIndexOf('.') < 0
-		    		) {
-			    		path = this.addFolder(path, pathparts[j]);
+				
+		    	var u = e.uri = this.parseUri( e.url ), 
+					// add hostname as root folder
+		    	    path = this.addFolder( '', u.host ),
+				    pathparts = u.path.split( '/' );
+				
+		    	for ( j = 0; j < pathparts.length; j++ ) {
+		    		if ( pathparts[ j ] &&
+		    			 // It's a file because it has an extension.
+		    			 // Could improve this one :)
+		    			 pathparts[ j ].lastIndexOf( '.' ) < 0 ) {
+			    		path = this.addFolder( path, pathparts[ j ] );
 		    		}
 		    	}
 		    	e.parentId = path;
-		    	this.urlset[i] = new Aloha.RepositoryDocument(e);
+		    	this.urlset[ i ] = new Aloha.RepositoryDocument( e );
 		    }
-		
-		    // repository name
+			
 		    this.repositoryName = 'Linklist';
 		},
 		
-		addFolder: function (path, name) {
-		
-			var type = path ? 'folder' : 'hostname';
-			var p = path ? path + '/' + name : name;
-		
-			if ( name && !this.folder[p] ) {
-				this.folder[p] = new Aloha.RepositoryFolder({
-						id: p,
-						name: (name)?name:p,
-						parentId: path,
-						type: 'host',
-						repositoryId: this.repositoryId
-				});
+		addFolder: function ( path, name ) {
+			var type = path ? 'folder' : 'hostname',
+			    p = path ? path + '/' + name : name;
+			
+			if ( name && !this.folder[ p ] ) {
+				this.folder[ p ] = new Aloha.RepositoryFolder( {
+					id: p,
+					name: ( name ) ? name : p,
+					parentId: path,
+					type: 'host',
+					repositoryId: this.repositoryId
+				} );
 			}
+			
 			return p;
 		},
 		
@@ -104,16 +101,18 @@ function(jQuery, repository, i18nCore) {
 		 * Searches a repository for object items matching query if objectTypeFilter.
 		 * If none found it returns null.
 		 */
-		query: function( p, callback ) {
+		query: function ( p, callback ) {
 			// Not supported; filter, orderBy, maxItems, skipcount, renditionFilter
 			var r = new RegExp( p.queryString, 'i' );
-			var d = this.urlset.filter( function( e, i, a ) {
+			
+			var d = this.urlset.filter( function ( e, i, a ) {
 				return (
 					( !p.queryString || e.name.match( r ) || e.url.match( r ) ) &&
 					( !p.objectTypeFilter || ( !p.objectTypeFilter.length ) || jQuery.inArray( e.type, p.objectTypeFilter ) > -1 ) &&
 					true //( !p.inFolderId || p.inFolderId == e.parentId )
 				);
 			} );
+			
 			callback.call( this, d );
 		},
 		
@@ -133,7 +132,7 @@ function(jQuery, repository, i18nCore) {
 					d.push( this.folder[ e ] );
 				}
 			}
-
+			
 			callback.call( this, d );
 		},
 		
@@ -144,27 +143,27 @@ function(jQuery, repository, i18nCore) {
 		parseUri: function(str) {
 			var	o = {
 					strictMode: false,
-					key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
+					key: [ "source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
 					q:   {
-						name:   "queryKey",
+						name: "queryKey",
 						parser: /(?:^|&)([^&=]*)=?([^&]*)/g
 					},
 					parser: {
 						strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
-						loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+						loose: /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
 					}
 				},
 				m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
 				uri = {},
 				i   = 14;
-		
+			
 			while (i--) uri[o.key[i]] = m[i] || "";
-		
+			
 			uri[o.q.name] = {};
 			uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
 				if ($1) uri[o.q.name][$1] = $2;
 			});
-		
+			
 			return uri;
 		},
 		
@@ -174,19 +173,22 @@ function(jQuery, repository, i18nCore) {
 		 * @param callback {function} callback function
 		 * @return {GENTICS.Aloha.Repository.Object} item with given id
 		 */
-		getObjectById: function (itemId, callback) {
-			var d = [];
-		
-			for (var i = 0; i < this.urlset.length; i++) {
-				if (this.urlset[i].id == itemId) {
-					d.push(this.urlset[i]);
+		getObjectById: function ( itemId, callback ) {
+			var i = 0,
+			    l = this.urlset.length,
+			    d = [];
+			
+			for ( ; i < l; i++ ) {
+				if ( this.urlset[ i ].id == itemId ) {
+					d.push( this.urlset[ i ] );
 				}
 			}
-		
-			callback.call(this, d);
+			
+			callback.call( this, d );
+			
 			return true;
 		}
+		
+} ) )();
 
-}))();
-
-});
+} );
