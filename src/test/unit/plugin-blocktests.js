e6f70cb7d0328050112a1f3e4034f2bcbb4051cc
Sebastian Kurfuerst <sebastian@typo3.org>
2011-11-23 08:40:42 +0100

    Testcases for Cut / Paste

--- src/test/unit/plugin-blocktests.js
+++ src/test/unit/plugin-blocktests.js
@@ -273,7 +273,7 @@ function( TestUtils) {
 				exclude   : false,
 				desc      : 'Copy/Paste Setup',
 				start     : '<div class="alohaContent"><p><b>Some</b> text before<span id="myDefaultBlock" data-foo="Bar" data-something="Bar">Please click me and press <b>ctrl/cmd+c</b></span>Some text after</p><p class="pasteTarget"><b>Please place the</b> cursor HERE &gt;&lt; and press cmd/crtl v</p><p><b>Some</b> more text</p></div>',
-				async: true,
+				async     : true,
 				operation : function(testContainer, testcase) {
 
 					jQuery('.alohaContent').aloha();
@@ -326,6 +326,82 @@ function( TestUtils) {
 										strictEqual(this.actual, this.expected, this.message);
 									});
 								})
+								start();
+							}, 400);
+							jQuery(document).unbind('keydown', keyDownListener);
+						}
+					};
+					jQuery(document).keydown(keyDownListener);
+				}
+			},
+
+			{ module : 'Cut/Paste' },
+			///////////////////////////////////////////////////////////////////////
+
+			{
+				exclude   : false,
+				desc      : 'Cut/Paste Setup',
+				start     : '<div class="alohaContent"><p class="src"><b>Some</b> text before<span id="myDefaultBlock" data-foo="Bar" data-something="Bar">Please click me and press<b>ctrl/cmd+x (CUT)</b></span>Some text after</p><p class="pasteTarget"><b>Please place the</b> cursor HERE &gt;&lt; and press cmd/crtl v</p><p><b>Some</b> more text</p></div>',
+				async     : true,
+				operation : function(testContainer, testcase) {
+
+					jQuery('.alohaContent').aloha();
+
+					jQuery('#myDefaultBlock').alohaBlock({
+						'aloha-block-type': 'DefaultBlock'
+					});
+
+					var testTimeout = window.setTimeout(function() {
+						test('Cut/Paste tests not run', function() {
+							ok(false, 'Manual cut/paste tests were not run');
+						});
+						start();
+					}, 10000);
+					var keyDownListener;
+					keyDownListener = function(e) {
+						if (e.which === 86) { // v pressed (ctrl-v)
+							// We wait a little to make sure the new block has been created correctly
+							window.setTimeout(function() {
+								window.clearTimeout(testTimeout);
+								var testResults = [
+									{
+										actual: (function() {
+											var numberOfIdsFound = 0;
+											jQuery('span', testContainer).each(function() {
+												if (jQuery(this).is('#myDefaultBlock')) {
+													numberOfIdsFound++;
+												}
+											});
+											return numberOfIdsFound;
+										})(),
+										expected: 0,
+										message: 'the old ID should not be inside the DOM anymore after cutting'
+									},
+									{
+										 // We use lower case here because IE8 and lower uses uppercase tags... argh!!
+										actual: jQuery('.aloha-block', testContainer)[0].innerHTML.toLowerCase(),
+										expected: '<span class="aloha-block-handle aloha-block-draghandle"></span>Please click me and press<b>ctrl/cmd+x (CUT)</b>'.toLowerCase(),
+										message: 'The block markup is as expected'
+									},
+									{
+										actual: jQuery('br', testContainer).length,
+										expected: 0,
+										message: 'The content should not have any superfluous <br> tags inserted'
+									},
+									// We use lower case here because IE8 and lower uses uppercase tags... argh!!
+									{
+										actual: jQuery('p.src', testContainer).html().toLowerCase(),
+										expected: '<b>Some</b> text beforeSome text after'.toLowerCase(),
+										message: 'The element should be removed from the source'
+									},
+								];
+
+								test('Cut/Paste works with inline blocks', function() {
+									jQuery.each(testResults, function() {
+										strictEqual(this.actual, this.expected, this.message);
+									});
+								})
+								// TODO: make sure the old stuff is removed
 								start();
 							}, 400);
 							jQuery(document).unbind('keydown', keyDownListener);
