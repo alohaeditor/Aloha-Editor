2f61fea4afe48e0ff9061e93327460d1177ea85f
Sebastian Kurfuerst <sebastian@typo3.org>
2011-11-24 09:29:37 +0100

    code cleanup

--- src/demo/block/blockdemo/lib/blockdemo-plugin.js
+++ src/demo/block/blockdemo/lib/blockdemo-plugin.js
@@ -17,10 +17,7 @@ define([
 			BlockManager.registerBlockType('ProductTeaserBlock', block.ProductTeaserBlock);
 			BlockManager.registerBlockType('CompanyBlock', block.CompanyBlock);
 			BlockManager.registerBlockType('EditableProductTeaserBlock', block.EditableProductTeaserBlock);
-			//BlockManager.registerBlockType('VCardBlock', block.VCardBlock);
-			BlockManager.registerBlockType('CustomHandleBlock', block.CustomHandleBlock);
-			BlockManager.registerBlockType('TwoColumnBlock', block.TwoColumnBlock);
-
+			
 			BlockManager.registerBlockType('ImageBlock', block.ImageBlock);
 			BlockManager.registerBlockType('EditableImageBlock', block.EditableImageBlock);
 			BlockManager.registerBlockType('NewsBlock', block.NewsBlock);
