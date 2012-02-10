67f505201e10368b09bf92a1798ac0edc52bf34d
Marcus Ross <marcus.ross@zahlenhelfer.de>
2012-02-10 18:21:39 +0100

    Added swedish translation.

---
+++ src/lib/aloha/nls/sw/i18n.js
@@ -0,0 +1,10 @@
+define( {
+	'floatingmenu.tab.format': 'formatera',
+	'floatingmenu.tab.insert': 'infoga',
+	'yes': 'ja',
+	'no': 'nej',
+	'cancel': 'avbryta',
+	'repository.no_item_found': 'ingen posten har hittats.',
+	'repository.loading': 'laddar',
+	'repository.no_items_found_yet': 'ingen inskrivning har hittats...'
+} );
