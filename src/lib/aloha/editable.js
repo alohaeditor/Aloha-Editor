14da350a883599c773f79c45d8931006078398a2
Petro Salema <p.salema@gentics.com>
2012-05-08 09:01:12 +0200

    Circumventing a IE7 browser crash caused by cloned nodes used in `getContents()'.

--- src/lib/aloha/editable.js
+++ src/lib/aloha/editable.js
@@ -693,7 +693,11 @@ define( [
 		 * @return contents of the editable
 		 */
 		getContents: function( asObject ) {
-			var clonedObj = this.obj.clone( false );
+			// Cloned nodes are problematic in IE7.  When trying to read/write
+			// to them, they can sometimes cause the browser to crash.
+			//var clonedObj = this.obj.clone( false );
+
+			var clonedObj = jQuery(this.obj[0].outerHTML);
 
 			// do core cleanup
 			clonedObj.find( '.aloha-cleanme' ).remove();
