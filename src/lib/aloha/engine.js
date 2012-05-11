2d520e4f35e7582bf8baafed60d5e843371da2ea
Petro Salema <p.salema@gentics.com>
2012-05-11 11:29:20 +0200

    Implementing work-around for IE7 bug in `isCollapsedLineBreak()'.
    The IE7 behaviour of getAttribute() is buggy in that it differs from the
    documentation.  According to the DOM Level 2 documentation at:
    http://msdn.microsoft.com/en-us/library/ie/ms536429(v=vs.85).aspx, the return
    value of getAttribute() should be a string, interger, or boolean.  But when one
    queries for node.getAttribute('style'), what is actually returned is not a
    string, as one might expect, but an object.  The returned object is in fact the
    style *object*.  I.e., node.style === node.getAttribute('style') .

--- src/lib/aloha/engine.js
+++ src/lib/aloha/engine.js
@@ -862,26 +862,27 @@ function isCollapsedLineBreak(br) {
 		ref = ref.parentNode;
 	}
 
-	var refStyle = $_( ref ).hasAttribute("style") ? ref.getAttribute("style") : null;
-	ref.style.height = "auto";
-	ref.style.maxHeight = "none";
-	ref.style.minHeight = "0";
-	var space = document.createTextNode("\u200b");
+	var origStyle = {
+		height: ref.style.height,
+		maxHeight: ref.style.maxHeight,
+		minHeight: ref.style.minHeight
+	};
+
+	ref.style.height = 'auto';
+	ref.style.maxHeight = 'none';
+	ref.style.minHeight = '0';
+	var space = document.createTextNode('\u200b');
 	var origHeight = ref.offsetHeight;
 	if (origHeight == 0) {
-		throw "isCollapsedLineBreak: original height is zero, bug?";
+		throw 'isCollapsedLineBreak: original height is zero, bug?';
 	}
 	br.parentNode.insertBefore(space, br.nextSibling);
 	var finalHeight = ref.offsetHeight;
 	space.parentNode.removeChild(space);
-	if (refStyle === null) {
-		// Without the setAttribute() line, removeAttribute() doesn't work in
-		// Chrome 14 dev.  I have no idea why.
-		ref.setAttribute("style", "");
-		ref.removeAttribute("style");
-	} else {
-		ref.setAttribute("style", refStyle);
-	}
+
+	ref.style.height = origStyle.height;
+	ref.style.maxHeight = origStyle.maxHeight;
+	ref.style.minHeight = origStyle.minHeight;
 
 	// Allow some leeway in case the zwsp didn't create a whole new line, but
 	// only made an existing line slightly higher.  Firefox 6.0a2 shows this
@@ -892,6 +893,7 @@ function isCollapsedLineBreak(br) {
 // "An extraneous line break is a br that has no visual effect, in that
 // removing it from the DOM would not change layout, except that a br that is
 // the sole child of an li is not extraneous."
+
 //
 // FIXME: This doesn't work in IE, since IE ignores display: none in
 // contenteditable.
