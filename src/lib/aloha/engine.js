alohaeditor/Aloha-Editor

commit 036530f4783880248e61a028de06a577c382de92

fix to delete first chars of text where are whitespaces in the source at the start of that text

src/lib/aloha/engine.js
@@ -6252,6 +6252,7 @@ commands["delete"] = {
 
 		// collapse whitespace sequences
 		collapseWhitespace(node, range);
+		offset = range.startOffset;
 
 		// "If node is a Text node and offset is not zero, call collapse(node,
 		// offset) on the Selection. Then delete the contents of the range with
@@ -6845,6 +6846,7 @@ commands.forwarddelete = {
 
 		// collapse whitespace in the node, if it is a text node
 		collapseWhitespace(node, range);
+		offset = range.startOffset;
 
 		// "If node is a Text node and offset is not node's length:"
 		if (node.nodeType == $_.Node.TEXT_NODE

commit 5b9ccc5194c13b0437876bf2c3dd6f151da725de

Removing errors in delete and forwarddelete algorithms that were introduced with commit 036530f4783880248e61a028de06a577c382de92.  We should look for an alternative way to address the problem which that commit was attempting to solve.

src/lib/aloha/engine.js
@@ -6282,7 +6282,6 @@ commands["delete"] = {
 
 		// collapse whitespace sequences
 		collapseWhitespace(node, range);
-		offset = range.startOffset;
 
 		// "If node is a Text node and offset is not zero, call collapse(node,
 		// offset) on the Selection. Then delete the contents of the range with
@@ -6890,7 +6889,6 @@ commands.forwarddelete = {
 
 		// collapse whitespace in the node, if it is a text node
 		collapseWhitespace(node, range);
-		offset = range.startOffset;
 
 		// "If node is a Text node and offset is not node's length:"
 		if (node.nodeType == $_.Node.TEXT_NODE