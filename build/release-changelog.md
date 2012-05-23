c3e9034811b1f83f4a71c8dabe941704fd2d3e34
Johannes Schüth <j.schueth@gentics.com>
2012-05-23 10:54:58 +0200

    Reverted 14da350a883599c773f79c45d8931006078398a2. The old IE7 workaround removed all event handlers once getContents() was called. The workaround was not browser specific. This caused other defects within IE9. The new workaround is browser specific. We now remove the expando attribute when copying attributes. The reading of the expando attribute name somehow caused a bug in ie7 to surface. A following clone call would crash ie7.

--- build/release-changelog.md
+++ build/release-changelog.md
@@ -1,1 +1,2 @@
 - **BUG**: browser-plugin: The browser will now open at a more centered position.
+- **BUG**: core: A IE7 crash workaround was reverted because it caused all eventhandlers to be lost when getContents() was invoked. The new workaround will remove the jquery expando attributes in IE7 for some elements.
--- src/lib/aloha/engine.js
+++ src/lib/aloha/engine.js
@@ -1423,7 +1423,21 @@ function movePreservingRanges(node, newParent, newIndex, range) {
  * @param {dom} newElement The new DOM element which will get the attributes of the source DOM element
  * @return void
  */
-function copyAttributes( element,  newElement ) {
+function copyAttributes( element, newElement ) {
+
+	// This is an IE7 workaround. We identified three places that were connected 
+	// to the mysterious ie7 crash:
+	// 1. Add attribute to dom element (Initialization of jquery-ui sortable)
+	// 2. Access the jquery expando attribute. Just reading the name is 
+	//    sufficient to make the browser vulnerable for the crash (Press enter)
+	// 3. On editable blur the Aloha.editables[0].getContents(); gets invoked.
+	//    This invokation somehow crashes the ie7. We assume that the access of 
+	//    shared expando attribute updates internal references which are not 
+	//    correclty handled during clone(); 
+	if ( jQuery.browser === 'msie' && jQuery.browser.version >=7 && typeof element.attributes[jQuery.expando] !== 'undefined' ) {
+		jQuery(element).removeAttr(jQuery.expando);
+	}
+
 	for ( var i = 0; i < element.attributes.length; i++ ) {
 		if ( typeof newElement.setAttributeNS === 'function' ) {
 			newElement.setAttributeNS( element.attributes[i].namespaceURI, element.attributes[i].name, element.attributes[i].value );
