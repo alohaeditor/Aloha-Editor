0c3f9da6134bf33479348b3510d02b1d2d7580de
Rene Kapusta <rene@evo42.net>
2012-05-29 14:32:11 +0200

    image plugin: resize with pressed mouse button / shift key

--- src/plugins/common/image/lib/image-plugin.js
+++ src/plugins/common/image/lib/image-plugin.js
@@ -834,6 +834,7 @@ define([
 					// Inital click on images will be handled here
 					// editable.obj.find('img').attr('_moz_resizing', false);
 					// editable.obj.find('img').contentEditable(false);
+					// @todo there's an issue when pressing the mouse button / shift key and release it... resizing does not stop on certain conditions. see: https://getsatisfaction.com/aloha_editor/topics/image_resizing-yfzk8
 					editable.obj.delegate( 'img', 'mouseup', function (event) {
 						that.clickImage(event);
 						event.stopPropagation();
