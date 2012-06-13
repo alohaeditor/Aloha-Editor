fcfa12c3dd96553054015b2649029c49b0c8038e
Clemens Prerovsky <c.prerovsky@gentics.com>
2012-06-13 17:09:54 +0200

    implemented config per editable for formatless paste plugin

--- src/plugins/extra/formatlesspaste/lib/formatlesspaste-plugin.js
+++ src/plugins/extra/formatlesspaste/lib/formatlesspaste-plugin.js
@@ -60,33 +60,49 @@ function(Aloha, Plugin, jQuery, FloatingMenu, FormatlessPasteHandler, ContentHan
 		init: function() {
 			var that = this;
 
-			this.registerFormatlessPasteHandler();
-
+			// look for old configuration directly in settings
 			if ( typeof this.settings.formatlessPasteOption !== 'undefined') {
 				this.formatlessPasteOption = this.settings.formatlessPasteOption;
 			}
-
+			
 			if ( typeof this.settings.strippedElements !== 'undefined') {
 				this.strippedElements = this.settings.strippedElements;
 			}
-
+			
+			// look for newer config in settings.config
+			if (this.settings.config) {
+				if (this.settings.config.formatlessPasteOption) {
+					this.formatlessPasteOption = this.settings.config.formatlessPasteOption;
+				}
+				if (this.settings.config.strippedElements) {
+					this.strippedElements = this.settings.config.strippedElements;
+				}
+			}
+			
 			if ( this.formatlessPasteOption ) {
 				this.registerFormatlessPasteHandler(); 
+
+				var formatlessPasteHandlerLastState;
+				Aloha.bind( 'aloha-editable-activated', function( event, params) {
+					var config = that.getEditableConfig( params.editable.obj );
+					if ( !config ) {
+						return;
+					}
+					if ( config.strippedElements ) {
+						FormatlessPasteHandler.strippedElements = config.strippedElements;
+					}
+					if ( config.formatlessPasteOption ) {
+						that.formatlessPasteButton.show();
+						if ( typeof formatlessPasteHandlerLastState !== 'undefined' ) {
+							FormatlessPasteHandler.enabled = formatlessPasteHandlerLastState;
+						}
+					} else {
+						that.formatlessPasteButton.hide();
+						formatlessPasteHandlerLastState = FormatlessPasteHandler.enabled;
+						FormatlessPasteHandler.enabled = false;
+					}
+				});
 			};
-
-			Aloha.bind( 'aloha-editable-activated', function( event, params) {
-				var config = that.getEditableConfig( params.editable.obj );
-				if ( config && config.formatlessPasteOption ) {
-					that.formatlessPasteOption = true;
-					that.registerFormatlessPasteHandler(); 
-				};
-
-				if ( config && typeof config.strippedElements !== 'undefined') {
-					that.strippedElements = config.strippedElements;
-				}
-				
-			});
-
 		},
 
 		/**
