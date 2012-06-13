d8f5019afecf9d432252d0dbaa21d0c01f0e22be
Tobias Steiner <t.steiner@gentics.com>
2012-06-13 14:59:50 +0200

    Adds message ui skeleton for table-plugin port

---
+++ src/plugins/common/ui/lib/message.js
@@ -0,0 +1,21 @@
+define([
+	"aloha/jquery",
+	"ui/component"
+],
+function( jQuery, Component ) {
+
+	return {
+		'confirm': function(props) {
+			//props.title
+			//props.text
+			//props.yes function
+			//props.no function
+			//maybe props.close function
+		},
+		'alert': function(props) {
+			//props.title
+			//props.text
+			//maybe props.close fuction 
+		}
+	};
+});
