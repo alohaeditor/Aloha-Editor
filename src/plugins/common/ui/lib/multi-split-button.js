a7b3ebea154821c2d1ab78ad6b9231131121b8f2
Tobias Steiner <t.steiner@gentics.com>
2012-06-13 14:38:45 +0200

    Adds a basic implementation of the multi-split-button

--- src/plugins/common/ui/lib/multi-split-button.js
+++ src/plugins/common/ui/lib/multi-split-button.js
@@ -1,10 +1,45 @@
-define(["aloha/core", "aloha/jquery", "ui/component", "aloha/repositorymanager", 'ui/vendor/jquery-ui-autocomplete-html'], function(Aloha, $, Component, RepositoryManager){
+define(["aloha/core",
+		"aloha/jquery",
+		"ui/component",
+		"ui/multiSplit"],
+function(Aloha, $, Component, MultiSplit){
 
 	function MultiSplitButton(props){
+
+		Component.define(props.name, MultiSplit, {
+			getButtons: function() {
+				return makeButtonsFromOldStyleProps(props, false);
+			},
+			getItems: function() {
+				return makeButtonsFromOldStyleProps(props, true);
+			}
+		});
+
+		var activeItem = null;
+
+		function makeButtonsFromOldStyleProps(props, wide) {
+			var buttons = [];
+			$.each(props.items, function(_, item){
+				if ( !!item.wide != wide) {
+					return;
+				}
+				buttons.push({
+					label: item.tooltip,
+					icon: item.iconClass,
+					click: item.click,
+					isActive: function(){
+						// TODO return activeItem === item.name;
+					}
+				});
+			});
+			return buttons;
+		}
+
 		return {
 			showItem: function(){
 			},
-			setActiveItem: function(){
+			setActiveItem: function(name){
+				activeItem = name;
 			}
 		};
 	}
