34279805bbf641bda06c718a60c16d90ebdd7316
Tobias Steiner <t.steiner@gentics.com>
2012-06-23 20:01:51 +0200

    Fixes autocomplete component causes javascript errors
    The autocomplete component isn't compatible with the menu
    implementation from the dev branch.

--- src/plugins/extra/ribbon/lib/ribbon.js
+++ src/plugins/extra/ribbon/lib/ribbon.js
@@ -1,18 +1,7 @@
 define([
- 'jquery',
- 'order!aloha/jquery-ui',
-/* The menu and menubar versions distributed with Aloha
-  * (jquery-ui-1.9m6) have a bug. See jquery.ui.menu for more
-  * information.
-  * TODO: these includes should be removed once the bug is fixed in the
-  * jquery-ui version released with Aloha */
- 'order!ribbon/vendor/jquery.ui.menu',
- 'order!ribbon/vendor/jquery.ui.menubar',
- /* TODO fixing the style is still an open issue. If the new CSS files
-  * are not needed, remove them.
- 'css!ribbon/vendor/jquery.ui.menu.css',
- 'css!ribbon/vendor/jquery.ui.menubar.css',*/
- 'order!css!./css/ribbon.css'
+	'jquery',
+	'aloha/jquery-ui',
+	'css!./css/ribbon.css'
 ], function($){
 
 	function onSelect(event, ui) {
