298d75f00f8ab212cf2ee63aa39920cfeb9b8cc9
Petro Salema <p.salema@gentics.com>
2012-06-28 13:00:31 +0200

    Reducing the z-index of the toolbar.

--- src/plugins/common/ui/lib/toolbar.js
+++ src/plugins/common/ui/lib/toolbar.js
@@ -173,7 +173,7 @@ function (
 				});
 			}
 
-			this.$element.css('z-index', 999999).draggable({
+			this.$element.css('z-index', 9999).draggable({
 				'distance': 20,
 				'stop': function (event, ui) {
 					Toolbar.setFloatingPosition(ui.position);
