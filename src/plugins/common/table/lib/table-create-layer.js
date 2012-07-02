5d8c12afc391b2fff55722d8e6eaafdbdd7a3226
Tobias Steiner <ts@girigiri-software.com>
2012-07-02 14:54:32 +0200

    Removes debugging code

--- src/plugins/common/table/lib/table-create-layer.js
+++ src/plugins/common/table/lib/table-create-layer.js
@@ -116,7 +116,6 @@ function (jQuery) {
 			// button itself or a descendant of the button, hide the
 			// layer.
 			if (that.visible && !(e.target === that.get('target')[0] || jQuery.contains(that.get('target')[0], e.target))) {
-				debugger;
 				that.hide();
 			}
 		});
