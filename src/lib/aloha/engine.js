9927cf3f6088d8799e376922d2326a4cc0fc1da3
Tobias Steiner <ts@girigiri-software.com>
2012-07-13 00:41:39 +0200

    Microoptimization
    Only check firstChild and lastChild in nodeList once

--- src/lib/aloha/engine.js
+++ src/lib/aloha/engine.js
@@ -5571,27 +5571,27 @@ function splitParent(nodeList, range) {
 		removeExtraneousLineBreaksBefore(originalParent);
 	}
 
+	var firstChildInNodeList = jQuery.inArray(originalParent.firstChild, nodeList) != -1;
+	var lastChildInNodeList = jQuery.inArray(originalParent.lastChild, nodeList) != -1;
+
 	// "If the first child of original parent is in node list, and original
 	// parent follows a line break, set follows line break to true. Otherwise,
 	// set follows line break to false."
-	var followsLineBreak_ = jQuery.inArray(originalParent.firstChild, nodeList) != -1
-		&& followsLineBreak(originalParent);
+	var followsLineBreak_ = firstChildInNodeList && followsLineBreak(originalParent);
 
 	// "If the last child of original parent is in node list, and original
 	// parent precedes a line break, set precedes line break to true.
 	// Otherwise, set precedes line break to false."
-	var precedesLineBreak_ = jQuery.inArray(originalParent.lastChild, nodeList) != -1
-		&& precedesLineBreak(originalParent);
+	var precedesLineBreak_ = lastChildInNodeList && precedesLineBreak(originalParent);
 
 	// "If the first child of original parent is not in node list, but its last
 	// child is:"
-	if (jQuery.inArray(originalParent.firstChild, nodeList) == -1
-	&& jQuery.inArray(originalParent.lastChild, nodeList) != -1) {
+	if (!firstChildInNodeList && lastChildInNodeList) {
 		// "For each node in node list, in reverse order, insert node into the
 		// parent of original parent immediately after original parent,
 		// preserving ranges."
 		for (var i = nodeList.length - 1; i >= 0; i--) {
-      movePreservingRanges(nodeList[i], originalParent.parentNode, 1 + getNodeIndex(originalParent), range);
+			movePreservingRanges(nodeList[i], originalParent.parentNode, 1 + getNodeIndex(originalParent), range);
 		}
 
 		// "If precedes line break is true, and the last member of node list
@@ -5611,7 +5611,7 @@ function splitParent(nodeList, range) {
 	}
 
 	// "If the first child of original parent is not in node list:"
-	if (jQuery.inArray(originalParent.firstChild, nodeList) == -1) {
+	if (!firstChildInNodeList) {
 		// "Let cloned parent be the result of calling cloneNode(false) on
 		// original parent."
 		var clonedParent = originalParent.cloneNode(false);
@@ -5634,15 +5634,15 @@ function splitParent(nodeList, range) {
 	// "For each node in node list, insert node into the parent of original
 	// parent immediately before original parent, preserving ranges."
 	for (var i = 0; i < nodeList.length; i++) {
-    if(isHtmlElementInArray(originalParent.parentNode, ["LI"])){
-      var newParent = originalParent.parentNode.parentNode;
-      var newIndex = getNodeIndex(originalParent.parentNode) + 1;
+		if(isHtmlElementInArray(originalParent.parentNode, ["LI"])){
+			var newParent = originalParent.parentNode.parentNode;
+			var newIndex = getNodeIndex(originalParent.parentNode) + 1;
 
-      movePreservingRanges(nodeList[i], newParent, newIndex, range);
-      movePreservingRanges(originalParent, nodeList[i], -1, range);
-    } else {
-      movePreservingRanges(nodeList[i], originalParent.parentNode, getNodeIndex(originalParent), range);
-    }
+			movePreservingRanges(nodeList[i], newParent, newIndex, range);
+			movePreservingRanges(originalParent, nodeList[i], -1, range);
+		} else {
+			movePreservingRanges(nodeList[i], originalParent.parentNode, getNodeIndex(originalParent), range);
+		}
 	}
 
 	// "If follows line break is true, and the first member of node list does
