301b46036da22db3b3cb5b71e49a465a8a408cdd
Tobias Steiner <ts@girigiri-software.com>
2012-07-14 21:12:19 +0200

    Optimization

--- src/lib/aloha/engine.js
+++ src/lib/aloha/engine.js
@@ -382,11 +382,14 @@ function getFurthestAncestor(node) {
  */
 function isContained(node, range) {
 	var pos1 = getPosition(node, 0, range.startContainer, range.startOffset);
+	if (pos1 !== "after") {
+		return false;
+	}
 	var pos2 = getPosition(node, getNodeLength(node), range.endContainer, range.endOffset);
-
-	return getFurthestAncestor(node) == getFurthestAncestor(range.startContainer)
-		&& pos1 == "after"
-		&& pos2 == "before";
+	if (pos2 !== "before") {
+		return false;
+	}
+	return getFurthestAncestor(node) == getFurthestAncestor(range.startContainer);
 }
 
 /**
