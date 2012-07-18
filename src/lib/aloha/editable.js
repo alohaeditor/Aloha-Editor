06097d61bc3855ef1974cc1e2f7a269117e6bdcc
nka11 <nka11@nka.me>
2012-07-18 19:13:43 +0200

    one more check to avoid unexpected cursor move

--- src/lib/aloha/editable.js
+++ src/lib/aloha/editable.js
@@ -442,7 +442,7 @@ define( [
 			} else {
 				el = span;
 			}
-			if (jQuery( obj ).find("." + this.placeholderClass).length !== 0) return;
+			if (jQuery( "." + this.placeholderClass, obj).length !== 0) return;
 			jQuery( obj ).append( el.addClass( this.placeholderClass ) );
 			jQuery.each(
 				Aloha.settings.placeholder,
@@ -471,16 +471,18 @@ define( [
 		removePlaceholder: function( obj, setCursor ) {
 			var placeholderClass = this.placeholderClass,
 			    range;
-
+			if (jQuery( "." + this.placeholderClass, obj).length === 0) return;
 			// set the cursor // remove placeholder
 			if ( setCursor === true ) {
-				range = new Selection.SelectionRange();
-				range.startContainer = range.endContainer = obj.get( 0 );
-				range.startOffset = range.endOffset = 0;
 				window.setTimeout( function() {
+					//debugger;
+					range = new Selection.SelectionRange();
+					range.startContainer = range.endContainer = obj.get( 0 );
+					range.startOffset = range.endOffset = 0;
 					jQuery( '.' + placeholderClass, obj ).remove();
 					range.select();
-				}, 20 );
+				
+				}, 100 );
 			} else {
 				jQuery( '.' + placeholderClass, obj ).remove();
 			}
