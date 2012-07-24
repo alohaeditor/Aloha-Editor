ab8d035c181590cb3bd323601879121fb03dbcff
Tobias Steiner <ts@girigiri-software.com>
2012-07-24 10:40:47 +0200

    Fixes copy&paste crashes IE7

--- src/lib/aloha/selection.js
+++ src/lib/aloha/selection.js
@@ -2153,6 +2153,7 @@ function correctRange ( range ) {
 
 	function makeContextHtml(node, parents) {
 		var result = [],
+		    parent,
 		    len,
 		    i;
 		if (1 === node.nodeType) {
@@ -2161,7 +2162,20 @@ function correctRange ( range ) {
 			result.push('#' + node.nodeType);
 		}
 		for (i = 0, len = parents.length; i < len; i++) {
-			result.push(parents[i].cloneNode(false).outerHTML);
+			parent = parents[i];
+			if (parent.nodeName === 'BODY') {
+				// Although we limit the ancestors in most cases to the
+				// active editable, in some cases (copy&paste) the
+				// parent may be outside.
+				// On IE7 this means the following code may clone the
+				// HTML node too, which causes the browser to crash.
+				// On other browsers, this is just an optimization
+				// because the body and html elements should probably
+				// not be considered part of the context of an edit
+				// operation.
+				break;
+			}
+			result.push(parent.cloneNode(false).outerHTML);
 		}
 		return result.join('');
 	}
