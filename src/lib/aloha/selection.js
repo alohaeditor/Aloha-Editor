fbd89d63ad059cfe4591523066ef1e99c4934824
Tobias Steiner <ts@girigiri-software.com>
2012-07-25 16:27:23 +0200

    Fixes IE7 browser crash

--- src/lib/aloha/selection.js
+++ src/lib/aloha/selection.js
@@ -2156,14 +2156,14 @@ function correctRange ( range ) {
 		    parent,
 		    len,
 		    i;
-		if (1 === node.nodeType) {
+		if (1 === node.nodeType && node.nodeName !== 'BODY' && node.nodeName !== 'HTML') {
 			result.push(node.cloneNode(false).outerHTML);
 		} else {
 			result.push('#' + node.nodeType);
 		}
 		for (i = 0, len = parents.length; i < len; i++) {
 			parent = parents[i];
-			if (parent.nodeName === 'BODY') {
+			if (parent.nodeName === 'BODY' || parent.nodeName === 'HTML') {
 				// Although we limit the ancestors in most cases to the
 				// active editable, in some cases (copy&paste) the
 				// parent may be outside.
