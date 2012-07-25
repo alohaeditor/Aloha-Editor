126518efbdd4b163229dd1d5b41e24efbdcf1ec6
Johannes Schüth <j.schueth@gentics.com>
2012-07-25 12:07:27 +0200

    Removed animations from the cite plugin due to issues with leftover css attributes in the dom.

--- build/release-changelog.md
+++ build/release-changelog.md
@@ -2,3 +2,4 @@
 - **BUG**: table-plugin: Fixed the removal of the aloha-table-cell_active once a table gets deactivated.
 - **BUG**: wai-lang-plugin: The key combination ctrl+i caused a javascript error in IE8 when using the wai-lang-plugin together with the link plugin. This javascript error was now fixed.
 - **BUG**: core: A basic content handler was added to the core that will cleanup the dom and html that gets processed by the getContents method. Currently this basic contenthandler is used to remove attributes (tabindex, hidefocus, contenteditable) that were added when using the Internet Explorer.
+- **BUG**: cite-plugin: The css animations for the cite plugin were removed because they were poluting the dom with style attributes.
--- src/plugins/extra/cite/lib/cite-plugin.js
+++ src/plugins/extra/cite/lib/cite-plugin.js
@@ -25,8 +25,7 @@ function CiteClosure(Aloha, jQuery, Plugin, FloatingMenu, Format, domUtils,
 
 	var $ = jQuery,
 		ns  = 'aloha-cite',
-		uid = (new Date()).getTime(),
-		animating = false;
+		uid = (new Date()).getTime();
 
 	// namespaced classnames
 	var nsClasses = {
@@ -571,54 +570,6 @@ function CiteClosure(Aloha, jQuery, Plugin, FloatingMenu, Format, domUtils,
 			if (link) {
 				// Update link attribute
 				var el = jQuery(nsSel(uid)).attr('cite', link);
-
-				if (!animating) {
-					// Highlight animation for happy user.
-					var round = Math.round;
-					var from  = hex2rgb('#fdff9a');
-					var to    = hex2rgb('#fdff9a');
-
-					from.push(1);
-					to.push(0);
-
-					var diff = [ to[0] - from[0],
-								 to[1] - from[1],
-								 to[2] - from[2],
-								 to[3] - from[3] ];
-
-					var origBg = el[0].style.backgroundColor;
-					var origShadow = el[0].style.boxShadow;
-
-					el.css({
-						__tick: 0, // Our increment.
-						'background-color': 'rgba(' + from.join(',') + ')',
-						'box-shadow': '0 0 20px rgba(' + from.join(',') + ')'
-					});
-
-					animating = true;
-
-					el.animate({ __tick: 1}, {
-						duration: 500,
-						easing: 'linear',
-						step: function (val, fx) {
-							var rgba = [round(from[0] + diff[0] * val),
-							            round(from[1] + diff[1] * val),
-							            round(from[2] + diff[2] * val),
-							            from[3] + diff[3] * val];
-
-							jQuery(this).css({
-								'background-color': 'rgba(' + rgba.join(',') + ')',
-								'box-shadow': '0 0 ' + (20 * (1 - val)) +
-									'px rgba(' + from.join(',') + ')'
-							});
-						},
-						complete: function () {
-							animating = false;
-							this.style.backgroundColor = origBg;
-							this.style.boxShadow = origShadow;
-						}
-					});
-				}
 			}
 
 			// Update information in references list for this citation.
