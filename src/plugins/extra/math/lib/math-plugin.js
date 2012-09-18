aabfe547029f6e8b310023c6b89e956995f23ff0
Max Grossman <jmaxg3@gmail.com>
2012-09-18 18:18:41 -0500

    Add modified math plugin to mathedit branch

--- src/plugins/extra/math/lib/math-plugin.js
+++ src/plugins/extra/math/lib/math-plugin.js
@@ -11,9 +11,10 @@ function( plugin, $, ui, button, attributeField, scopes, floatingMenu )
         {
             wrapPrefix: 'eqprefix-'
         },
-        hotKey: { insertMath: 'ctrl+m' },
+        hotKey: { insertTexMath: 'ctrl+m', insertAsciiMath: 'ctrl+j', insertMLMath: 'ctrl+k' },
         init: function() 
         {
+            var editableObj = null;
             var self = this,
                 wrapPrefix = this.settings.wrapPrefix;
             
@@ -21,59 +22,109 @@ function( plugin, $, ui, button, attributeField, scopes, floatingMenu )
             var script0 = document.createElement("script");
             script0.type = "text/x-mathjax-config";
             $(script0).html( 'MathJax.Hub.Config({'
-                    + 'jax: ["input/TeX","input/MathML","output/SVG"],'
-                    + 'extensions: ["tex2jax.js","mml2jax.js","MathMenu.js","MathZoom.js"],'
-                    + 'tex2jax: { inlineMath: [["$","$"],["\\(","\\)"]] },'
+                    + 'jax: ["input/MathML", "input/TeX", "input/AsciiMath", "output/NativeMML"],'
+                    + 'extensions: ["asciimath2jax.js", "tex2jax.js","mml2jax.js","MathMenu.js","MathZoom.js"],'
+                    + 'tex2jax: { inlineMath: [["$","$"]] },'
+                    + 'asciimath2jax: { inlineMath: [["`", "`"]], delimiters: [["`","`"]] },'
                     + 'TeX: {'
                         + 'extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"], noErrors: { disabled: true }'
-                    + '}});');
-            //$(script0).html('MathJax.Hub.Config({ tex2jax: { inlineMath: [["$","$"],["\\(","\\)"]] } });');
+                    + '},'
+                    + 'AsciiMath: { noErrors: { disabled: true } }'
+                    + '});');
             
             var script = document.createElement("script");
             script.type = "text/javascript";
-            //script.src  = "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_SVG";
             script.src  = "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=default";
             var config = 'MathJax.Hub.Startup.onload();';
             $(script).html(config);
             
             document.getElementsByTagName("head")[0].appendChild(script0);
             document.getElementsByTagName("head")[0].appendChild(script);
-            
-            // -----
-            
-            //new MathMenu();
-            
-            // clicking eq
-            function editMath(evt)
-            {
-                self._mathEditInput.data('current-edit', this).show().val($(this).data('equation'));
+
+            var inChange = false;
+           
+            function onTexCharChange(evt, arg) {
+                if(inChange) return;
+                inChange = true;
+                var range = window.getSelection().getRangeAt(0);
+                var offset = range.startOffset;
+                var eqId = evt.currentTarget.id.substring(5);
+                var ele = $('#'+evt.currentTarget.id);
+                var leVal = ele.val() || ele.text();
+                var ch = leVal[offset];
+                if(ch == '{') {
+                    leVal = leVal.slice(0,offset+1)+'}'+leVal.slice(offset+1);
+                    /*
+                    if(ele.val()) {
+                        ele.val(leVal);
+                    } else {
+                        ele.text(leVal);
+                    }
+                    console.log(evt);
+                    */
+                }
+                MathJax.Hub.queue.Push(["Text", MathJax.Hub.getAllJax(eqId)[0],"\\displaystyle{"+leVal+"}"]);
+                inChange = false;
             }
-            
-            // changing eq in input
-            function changeMath(evt)
-            {
-                updateMath.call(this, null, $($(this).data('current-edit')).attr('id'));
-                $(this).val('');
+
+            function onAsciiCharChange(evt) {
+                var eqId = evt.currentTarget.id.substring(5);
+                var ele = $('#'+evt.currentTarget.id);
+                var leVal = ele.val() || ele.text();
+                MathJax.Hub.queue.Push(["Text", MathJax.Hub.getAllJax(eqId)[0],leVal]);
             }
-            
-            function updateMath(evt, eqId)
-            {
-                var leVal = $(this).val() || $(this).text();
+
+            function generateMathContainer(openDelimiter, closeDelimiter, charChangeFunction, initValue, editableObj) {
+                var newElId = wrapPrefix+cntEq;
+                var range = Aloha.Selection.getRangeObject();
+                var newMathEditContainer = $('<div id="edit-'+newElId+'" style="padding:2px;min-height:28px;border:1px solid green;-moz-border-radius: 4px;-webkit-border-radius: 4px;-khtml-border-radius: 4px;border-radius: 4px;background-color:white;">'+initValue+'</div>');
+                var newMathContainer = $('<div id="'+newElId+'" style="left;border:1px dotted grey">'+openDelimiter+closeDelimiter+'</div>');
                 
-                if( !eqId || !leVal ) return;
+                GENTICS.Utils.Dom.insertIntoDOM( newMathEditContainer, range, $( Aloha.activeEditable.obj ) );
+                GENTICS.Utils.Dom.insertIntoDOM( newMathContainer, range, $( Aloha.activeEditable.obj ) );
+                GENTICS.Utils.Dom.setCursorInto( newMathEditContainer[0] );
+                newMathEditContainer.bind('DOMCharacterDataModified', charChangeFunction);
+                newMathEditContainer.bind('DOMNodeInserted', charChangeFunction);
+                newMathEditContainer.hide();
+               
+                if(initValue == '') {
+                    MathJax.Hub.queue.Push(["Typeset", MathJax.Hub, newElId, function() { newMathEditContainer.show() }]);
+                } else {
+
+                    if(openDelimiter == '${') {
+
+                        MathJax.Hub.queue.Push(["Typeset", MathJax.Hub, newElId, function() { 
+                               newMathEditContainer.show(); 
+                               MathJax.Hub.queue.Push(["Text", MathJax.Hub.getAllJax(newElId)[0],"\\displaystyle{"+initValue+"}"]);
+                        }]);
+                    } else {
+
+                        MathJax.Hub.queue.Push(["Typeset", MathJax.Hub, newElId, function() { 
+                               newMathEditContainer.show(); 
+                               MathJax.Hub.queue.Push(["Text", MathJax.Hub.getAllJax(newElId)[0],initValue]);
+                        }]);
+                    }
+                }
+                                    
+                var blurout = function()
+                {
+                    newMathEditContainer.hide();
+                };
+
+                $(editableObj).on('blur focusout', blurout);
+                newMathEditContainer.on('focusout blur', blurout);
                 
-                console.log(arguments, eqId, $('#'+eqId), MathJax.Hub.getAllJax(eqId)[0]);
                 
+                newMathContainer.on('click', function()
+                {
+                    GENTICS.Utils.Dom.setCursorInto( newMathEditContainer[0] );
+                    newMathEditContainer.show();
+                });
                 
-                MathJax.Hub.queue.Push(["Typeset", MathJax.Hub, eqId, function()
-                { 
-                    MathJax.Hub.queue.Push(["Text", MathJax.Hub.getAllJax(eqId)[0],"\\displaystyle{"+leVal+"}"]);
-                }]);
-                
-                $('#'+eqId).data('equation', leVal).on('click.aloha-math', editMath );
-                
+                cntEq++;
+
             }
-            
+
             function toggleMath()
             {
                 if( Aloha.activeEditable ) 
@@ -83,10 +134,6 @@ function( plugin, $, ui, button, attributeField, scopes, floatingMenu )
                         return;
                     }
                     
-                    // this should work but it has some stupid preprocessing that sanitizes elements
-                    // in some content handler manager which limits the things we can insert 
-                    // Aloha.execCommand('insertHTML', false, $( self.settings.mathPlaceholder) );
-
                     // get text from selection
                     var leText = range.getText();
                     
@@ -94,15 +141,10 @@ function( plugin, $, ui, button, attributeField, scopes, floatingMenu )
                     
                     GENTICS.Utils.Dom.removeRange(range);
                     // make a new placeholder for the new equation
-                    GENTICS.Utils.Dom.insertIntoDOM( $('<span id="'+wrapPrefix+cntEq+'">${}$</span>'), range, $( Aloha.activeEditable.obj ) );
-                    leMathInput.val(leText).trigger('change', [wrapPrefix+cntEq]);
-                    cntEq++;
+
+                    generateMathContainer('${','}$', onTexCharChange, leText, editableObj);
                 }
             }
-            
-            var leMathInput = $('<input style="display:none" />');
-            leMathInput.on('change', updateMath );
-            leMathInput.appendTo('body');
             
             scopes.createScope('math', 'Aloha.empty');
             
@@ -112,20 +154,6 @@ function( plugin, $, ui, button, attributeField, scopes, floatingMenu )
                 icon: "M",
                 click: toggleMath
             });
-            
-            self._mathEdit = new attributeField
-            ({
-                label: 'Math',/*i18n.t('field.mathedit.label'),*/
-                tooltip: 'Math Edit', /*i18n.t('field.mathedit.tooltip'),*/
-                name: 'matheditSource',
-                scope: 'math'
-            });
-            
-            // hack cause I have no idea how this works, and there is no docs for it
-            self._mathEditInput = $(self._mathEdit.getInputElem());
-            self._mathEditInput.css({width: 150}).insertAfter(self._mathCtrl.element[0]);
-            self._mathEditInput.hide();
-            self._mathEditInput.on('change', changeMath);
             
             var parsedJax = false; 
             Aloha.bind('aloha-editable-activated', function (event, data) 
@@ -143,8 +171,7 @@ function( plugin, $, ui, button, attributeField, scopes, floatingMenu )
                                 elpr = $('#'+this.inputID+'-Frame').prevAll('.MathJax_Preview').eq(0),
                                 eqWrapper = $('<span id="'+wrapPrefix+cntEq+'" />').insertBefore(elpr)
                                     .append(elpr).append(elfr).append(el)
-                                    .data('equation', this.originalText)
-                                    .on('click.aloha-math', editMath );
+                                    .data('equation', this.originalText);
                             
                             cntEq++;
                         }); 
@@ -156,59 +183,22 @@ function( plugin, $, ui, button, attributeField, scopes, floatingMenu )
            
             Aloha.bind('aloha-editable-created', function (event, editable) 
             {
+                editableObj = editable.obj;
                 
-                var activeMathEdit = false;
-                editable.obj.bind('keydown', self.hotKey.insertMath, function() 
+                editable.obj.bind('keydown', self.hotKey.insertTexMath, function() 
                 {
-                    if( activeMathEdit !== false ) 
-                    {
-                        $('#edit-'+newElId).hide();
-                        clearInterval(activeMathEdit);
-                        activeMathEdit = false;
-                        return
-                    }
-                    
-                    var newElId = wrapPrefix+cntEq,
-                        range = Aloha.Selection.getRangeObject(),
-                        newMathEditContainer = $('<div id="edit-'+newElId+'" style="padding:2px;min-height:28px;border:1px solid green;-moz-border-radius: 4px;-webkit-border-radius: 4px;-khtml-border-radius: 4px;border-radius: 4px;background-color:white;"></div>'),
-                        newMathContainer = $('<div id="'+newElId+'" style="left;border:1px dotted grey">${}$</div>');
-                    
-                    GENTICS.Utils.Dom.insertIntoDOM( newMathEditContainer, range, $( Aloha.activeEditable.obj ) );
-                    GENTICS.Utils.Dom.insertIntoDOM( newMathContainer, range, $( Aloha.activeEditable.obj ) );
-                    GENTICS.Utils.Dom.setCursorInto( newMathEditContainer[0] );
-                    
-                    function startWatch()
-                    {
-                        activeMathEdit = setInterval(function()
-                        {
-                            updateMath.call(newMathEditContainer, null, newElId); 
-                        }, 2000);
-                    };
-                    startWatch();
-                    
-                    
-                    var blurout = function()
-                    {
-                        newMathEditContainer.hide();
-                        clearInterval(activeMathEdit);
-                        activeMathEdit = false;
-                    };
-                    $(editable.obj).on('blur focusout', blurout);
-                    newMathEditContainer.on('focusout blur', blurout);
-                    
-                    
-                    newMathContainer.on('click', function()
-                    {
-                        GENTICS.Utils.Dom.setCursorInto( newMathEditContainer[0] );
-                        newMathEditContainer.show();
-                        startWatch();
-                    });
-                    
-                    cntEq++;
-                    
+                    generateMathContainer('${','}$', onTexCharChange, '', editable.obj);
+                });
+
+                editable.obj.bind('keydown', self.hotKey.insertAsciiMath, function() 
+                {
+                    generateMathContainer('`','`', onAsciiCharChange, '', editable.obj);
+                });
+                editable.obj.bind('keydown', self.hotKey.insertMLMath, function() 
+                {
+                    generateMathContainer('<math>','</math>', onAsciiCharChange, '', editable.obj);
                 });
             });
-            
         }
     });
 });
