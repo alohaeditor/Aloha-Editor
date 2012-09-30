c54d6b90d267da869ad650032df194f2a16a71a4
Max Grossman <jmaxg3@gmail.com>
2012-09-30 01:43:40 -0500

    add back in caret placer

--- src/plugins/extra/math/lib/math-plugin.js
+++ src/plugins/extra/math/lib/math-plugin.js
@@ -1199,6 +1199,24 @@ function getSelectionText() { // from Tim Down at http://stackoverflow.com/quest
     return text;
   }
 
+  function placeCaretAtEnd(el) { // From Tim Down at http://stackoverflow.com/questions/4233265/contenteditable-set-caret-at-the-end-of-the-text-cross-browser
+    el.focus();
+    if (typeof window.getSelection != "undefined"
+            && typeof document.createRange != "undefined") {
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        range.collapse(false);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (typeof document.body.createTextRange != "undefined") {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.collapse(false);
+        textRange.select();
+    }
+  }
+
 function getFullStr(allChildren) {
     var str = '';
     for(var i = 0; i < allChildren.length; i++) {
