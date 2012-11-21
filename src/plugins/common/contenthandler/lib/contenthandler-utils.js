64c88de10dfd75a4d206b8484d7f9c09e7c7e28c
Petro Salema <p.salema@gentics.com>
2012-11-21 10:38:46 +0100

    Adding isProppedParagraph() among contenthander utility functions
    Fixes incomplete commit 8fdd5ca16c83bb27d9dd802360139b34e61e352a

--- src/plugins/common/contenthandler/lib/contenthandler-utils.js
+++ src/plugins/common/contenthandler/lib/contenthandler-utils.js
@@ -13,6 +13,37 @@
 define(['jquery'], function ($) {
 	'use strict';
 
+	/**
+	 * Checks whether the markup describes a paragraph that is propped by
+	 * a <br> tag but is otherwise empty.
+	 *
+	 * Will return true for:
+	 *
+	 * <p id="foo"><br class="bar" /></p>
+	 *
+	 * as well as:
+	 *
+	 * <p><br></p>
+	 *
+	 * @param {string} html Markup
+	 * @return {boolean} True if html describes a propped paragraph.
+	 */
+	function isProppedParagraph(html) {
+		var trimmed = $.trim(html);
+		if (!trimmed) {
+			return false;
+		}
+		var node = $('<div>' + trimmed + '</div>')[0];
+		var containsSingleP = node.firstChild === node.lastChild
+		                   && 'p' === node.firstChild.nodeName.toLowerCase();
+		if (containsSingleP) {
+			var kids = node.firstChild.children;
+			return (kids && 1 === kids.length &&
+					'br' === kids[0].nodeName.toLowerCase());
+		}
+		return false;
+	}
+
 	function wrapContent(content) {
 		if (typeof content === 'string') {
 			return $('<div>' + content + '</div>');
@@ -24,6 +55,7 @@ define(['jquery'], function ($) {
 	}
 
 	return {
-		wrapContent: wrapContent
+		wrapContent: wrapContent,
+		isProppedParagraph: isProppedParagraph
 	};
 });
