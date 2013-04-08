39f935276dcff03beec88a46c17666f90e13b9d3
Martin Schönberger <m.schoenberger@gentics.com>
2013-04-08 19:04:55 +0200

    Fix header-selection bugfix

--- src/plugins/common/table/lib/table-plugin.js
+++ src/plugins/common/table/lib/table-plugin.js
@@ -236,20 +236,20 @@ define([
 	 * @param {Aloha.Table} table the table-object for which the headers are to be set
 	 * @param {string} scope for which the header should be used (i.e. 'row' or 'column')
 	 */
-	function toggleHeaderStatus(selection, table, scope) {
+	function toggleHeaderStatus(table, scope) {
 		var	i,
 			j,
-			allHeaders = selection.isHeader(),
+			allHeaders = table.selection.isHeader(),
 			domCell, // representation of the cell in the dom
 			tableCell, // table-cell object
 			bufferCell; // temporary buffer
 
-		for (var i = 0; i < table.selection.selectedCells.length; i++) {
+		for (i = 0; i < table.selection.selectedCells.length; i++) {
 			domCell = table.selection.selectedCells[i];
 
 			// tries to match the current cell with a cell-object in the table
-			for (var j = 0; j < table.cells.length; j++) {
-				if ($(domCell)[0] === table.cells[j].obj[0]) {
+			for (j = 0; j < table.cells.length; j++) {
+				if (domCell === table.cells[j].obj[0]) {
 					cell = table.cells[j];
 					break;
 				}
@@ -258,23 +258,25 @@ define([
 			// the transformed dom objects are first stored in a buffer, and only applied to
 			// the table-cell-object if a match was found
 			if (allHeaders) {
-				bufferCell = Aloha.Markup.transformDomObject( domCell, 'td' ).removeAttr( 'scope' ).get(0);
+				bufferCell = Aloha.Markup.transformDomObject(domCell, 'td').removeAttr('scope').get(0);
 			} else {
-				bufferCell = Aloha.Markup.transformDomObject( domCell, 'th' ).attr( 'scope', scope ).get(0);
+				bufferCell = Aloha.Markup.transformDomObject(domCell, 'th').attr('scope', scope).get(0);
 			}
 
 			if (cell != null) {
 				// assign the changed dom-element to the table-cell
 				cell.obj[0] = bufferCell;
 
-				// reactivate the table-cell in order to bind events to the changed dom-object
+				// reactivate the table cell in order to bind events to the changed dom object
+				// TODO: re-attaching event-handlers should be factored out into a utility function
+				// so we don't have to do the whole activation/deactivation process for the cells
 				cell.deactivate();
 				cell.activate();
 			}
 
 			// uncommented code-segment, presumably added to force IE to target the wrapper
 			// on mouse-down by applying a timeout after event propagation
-			jQuery( table.selection.selectedCells[i] ).bind( 'mousedown', function ( jqEvent ) {
+			jQuery(table.selection.selectedCells[i]).bind('mousedown', function (jqEvent) {
 				var wrapper = jQuery(this).children('div').eq(0);
 				window.setTimeout(function () {
 					wrapper.trigger( 'focus' );
