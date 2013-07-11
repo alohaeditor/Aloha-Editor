75b77560f04aea5d3d26ab02d7dc4eb761d89c4a
Petro Salema <p.salema@gentics.com>
2013-07-11 18:33:48 +0200

    Don't use pop() but slice() to not mutate the array

--- tests/documentation-tests.js
+++ tests/documentation-tests.js
@@ -12,17 +12,17 @@ require(['../src/api'], function (Aloha) {
 			}, 500);
 		}
 	}(Aloha, Aloha.Maps.keys(Aloha), function (api, namespaces, runner, next) {
-		var namespace = namespaces.pop();
+		var namespace = namespaces.slice(0, 1);
 
 		test(namespace, function () {
-			var modules = Aloha.Maps.keys(api[namespace]);
-			var counter = modules.length;
+			var module = api[namespace];
+			var counter = Aloha.Maps.keys(module).length;
 
 			function proceed() {
 				next(api, namespaces.slice(1, namespaces.length), runner, next);
 			}
 
-			Aloha.Maps.forEach(api[namespace], function (obj, name) {
+			Aloha.Maps.forEach(module, function (obj, name) {
 				var type = typeof obj;
 
 				if ('function' !== type && 'object' !== type) {
@@ -32,16 +32,12 @@ require(['../src/api'], function (Aloha) {
 					return;
 				}
 
-				var dox = mandox(obj);
-
 				ok(
-					MANDOX_SIGNET.test(dox),
+					MANDOX_SIGNET.test(mandox(obj)),
 					'Expecting documentation for `' + name + (
 						'function' === type ? '()' : ''
 					) + '`'
 				);
-
-				// console.log('%s:\n%s', name, dox);
 
 				if (0 === --counter) {
 					proceed();
