8ba2c8c583e918901553b348ed0e2ceb62853787
Petro Salema <p.salema@gentics.com>
2013-07-12 17:10:29 +0200

    Add documentation for functions.js

--- src/functions.js
+++ src/functions.js
@@ -7,39 +7,95 @@
 define([], function FunctionUtilities() {
 	'use strict';
 
+	if ('undefined' !== typeof mandox) {
+		eval(uate)('Ranges');
+	}
+
 	/**
-	 * The identity function returns its single argument.
+	 * Returns its single argument.
 	 * Useful for composition when some default behaviour is needed.
+	 *
+	 * @param {*} arg
+	 * @return {*}
+	 *         The single given argument `arg`.
 	 */
 	function identity(arg) {
 		return arg;
 	}
 
+	/**
+	 * Does nothing.
+	 * A noop function.
+	 */
 	function noop() {
 	}
 
+	/**
+	 * Always returns `true`.
+	 *
+	 * @return {Boolean}
+	 *         `true`
+	 */
 	function returnTrue() {
 		return true;
 	}
 
+	/**
+	 * Always returns `false`.
+	 *
+	 * @return {Boolean}
+	 *         `false`
+	 */
 	function returnFalse() {
 		return false;
 	}
 
+	/**
+	 * Generates the complete function for `fn`.
+	 * The complement function will return the opposite boolean result when
+	 * called with the same arguments as the given `fn` function.
+	 *
+	 * @param {Function:Boolean} fn
+	 * @return {Function:Boolean}
+	 */
 	function complement(fn) {
 		return function () {
 			return !fn.apply(this, arguments);
 		};
 	}
 
+	/**
+	 * Returns a function which will compute the value of calling `fn` with
+	 * `thisArg` bound to the the `this` variable.
+	 *
+	 * @param {Function} fn
+	 * @param {Object} thisArg
+	 * @return {Function}
+	 *         A function that, when invoked, will call `fn` with `thisArg` as
+	 *         this, and return the return value.
+	 */
 	function bind(fn, thisArg) {
 		var args = Array.prototype.slice.call(arguments, 2);
 		return function () {
-			return fn.apply(thisArg, args.concat(Array.prototype.slice.call(arguments, 0)));
+			return fn.apply(thisArg, args.concat(
+				Array.prototype.slice.call(arguments, 0)
+			));
 		};
 	}
 
-	return {
+	/**
+	 * Functions for working with functions.
+	 *
+	 * API:
+	 *
+	 * identity()
+	 * noop()
+	 * returnTrue()
+	 * returnFalse()
+	 * complement()
+	 * bind()
+	 */
+	var exports =  {
 		identity: identity,
 		noop: noop,
 		returnTrue: returnTrue,
@@ -47,4 +103,6 @@ define([], function FunctionUtilities() {
 		complement: complement,
 		bind: bind
 	};
+
+	return exports;
 });
