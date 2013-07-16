fc5656ee58c7d138b26e0dc05dfe7177c2ff4ac2
Petro Salema <p.salema@gentics.com>
2013-07-16 13:12:21 +0200

    Change name of closure

--- src/strings.js
+++ src/strings.js
@@ -4,7 +4,7 @@
  * Copyright (c) 2010-2013 Gentics Software GmbH, Vienna, Austria.
  * Contributors http://aloha-editor.org/contribution.php 
  */
-define([], function StringsAPI() {
+define([], function StringsUtilities() {
 	'use strict';
 
 	if ('undefined' !== typeof mandox) {
