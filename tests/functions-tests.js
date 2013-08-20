772dadd48f6cd3a93486026b7733a8f2c5770eca
Petro Salema <p.salema@gentics.com>
2013-08-20 15:53:21 +0200

    Add tests for aloha.fn

---
+++ tests/functions-tests.js
@@ -0,0 +1,45 @@
+(function (aloha) {
+	'use strict';
+
+	var fn = aloha.fn;
+	var tested = [];
+
+    module('functions');
+
+	test('identity', function () {
+		tested.push('identity');
+		var func = function () {};
+		equal(func, fn.identity(func));
+	});
+
+	test('noop', function () {
+		tested.push('noop');
+		equal(fn.noop.toString(), 'function (){}');
+	});
+
+	test('returnTrue', function () {
+		tested.push('returnTrue');
+		equal(fn.returnTrue(), true);
+	});
+
+	test('returnFalse', function () {
+		tested.push('returnFalse');
+		equal(fn.returnFalse(), false);
+	});
+
+	test('complement', function () {
+		tested.push('complement');
+		equal(fn.complement(fn.returnFalse)(), true);
+	});
+
+	test('bind', function () {
+		tested.push('bind');
+		equal(fn.bind(function () {
+			return this.foo;
+		}, {foo: 'foo'})(), 'foo');
+	});
+
+	test('COVERAGE', function () {
+		testCoverage(equal, tested, fn);
+	});
+}(window.aloha));
