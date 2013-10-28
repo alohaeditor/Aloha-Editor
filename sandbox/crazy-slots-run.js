3a20425f6a7aa4f3cee3ee66f5045b33808aa3b7
Tobias Steiner <ts@girigiri.io>
2013-10-28 16:33:35 +0100

    Implement crazy-slots

---
+++ sandbox/crazy-slots-run.js
@@ -0,0 +1,102 @@
+require([
+	'../src/aloha',
+	'../src/editables',
+	'../src/arrays',
+	'../src/typing',
+	'../src/keys',
+	'../src/undo',
+	'../src/ranges',
+	'../src/crazy-slots'
+], function (
+	Aloha,
+	Editables,
+	Arrays,
+	Typing,
+	Keys,
+	Undo,
+	Ranges,
+	CrazySlots
+) {
+	'use strict';
+
+	function setVersion (version) {
+		var h1 = document.getElementsByTagName('h1')[0];
+		if (h1) {
+			h1.innerHTML = h1.innerHTML + ': <span style="color:#df215a">' + version + '</span>';
+		}
+	}
+
+	function main() {
+		var editor = {
+			settings: {
+				defaultBlockNodeName: 'div'
+			},
+			overrides: [],
+			editables: {}
+		};
+		var elem = document.getElementById('aloha-editable');
+		var editable = Editables.Editable(elem);
+		Editables.assocIntoEditor(editor, editable);
+		Undo.enter(editable.undoContext, {
+			meta: {type: 'external'},
+			partitionRecords: true
+		});
+		var mutations = [
+			{
+				probability: 10,
+				deletesRange: true,
+				mutate: function (elem, range) {
+					var texts = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet,', 'quodsi', 'deleniti', 'per', 'at,',
+					             'pro', 'aperiam', 'dissentiet', 'ad.', 'Hinc', 'nominati',
+					             'pri', 'no,', 'et',
+					             'vix', 'tacimates', 'maluisset', 'adversarium.', 'Et',
+					             'mea', 'etiam', 'legendos,',
+					             'posse', 'graeco', 'bonorum', 'per', 'in.',
+					             'Dicant', 'insolens', 'iracundia', 'ne', 'vis,', 'in',
+					             'eligendi', 'iudicabit', 'vis.'];
+					
+					var text = texts[CrazySlots.randomInt(0, texts.length - 1)];
+					if (CrazySlots.randomInt(0, 20) !== 0) {
+						Ranges.collapseToStart(range);
+					}
+					Typing.actions.insertText(range, text, editor);
+				}
+			}
+		];
+		var simpleActions = [
+			Keys.CODES.delete,
+			Keys.CODES.backspace,
+			Keys.CODES.enter,
+			'shift+' + Keys.CODES.enter,
+			'ctrl+' + Keys.CODES.undo,
+			'ctrl+shift+' + Keys.CODES.undo,
+			'ctrl+' + Keys.CODES.bold,
+			'ctrl+' + Keys.CODES.italic
+		];
+		var deletesRange = [
+			Keys.CODES.delete,
+			Keys.CODES.backspace,
+			Keys.CODES.enter,
+			'shift+' + Keys.CODES.enter
+		];
+		simpleActions.forEach(function (code) {
+			deletesRange: Arrays.contains(deletesRange, code),
+			mutations.push({
+				mutate: function (elem, range) {
+					Typing.actions[code](range, editor);
+				}
+			});
+		});
+		CrazySlots.run(editable.elem, mutations, {
+			runs: 10000,
+			wait: 0
+		});
+	}
+
+	if ('undefined' === typeof aloha) {
+		setVersion('src');
+	} else {
+		setVersion('build');
+	}
+	main()
+});
---
+++ src/crazy-slots.js
@@ -0,0 +1,96 @@
+/* crazy-slots.js is part of Aloha Editor project http://aloha-editor.org
+ *
+ * Aloha Editor is a WYSIWYG HTML5 inline editing library and editor.
+ * Copyright (c) 2010-2013 Gentics Software GmbH, Vienna, Austria.
+ * Contributors http://aloha-editor.org/contribution.php
+ */
+define(['arrays', 'ranges', 'dom', 'boundaries'], function CrazySlots(Arrays, Ranges, Dom, Boundaries) {
+	'use strict';
+
+	function constantly(value) {
+		return function () {
+			return value;
+		};
+	}
+
+	function repeat(value, n) {
+		var result = new Array(n);
+		var i;
+		for (i = 0; i < n; i++) {
+			result[i] = value;
+		}
+		return result;
+	}
+
+	function randomInt(min, max) {
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	}
+
+	function boundariesInElem(elem) {
+		var children = Dom.children(elem);
+		return children.map(function (node, i) {
+			return [elem, i];
+		}).concat([[elem, children.length]]);
+	}
+
+	function randomRange(elem, boundaryProbability, distance) {
+		var boundaries = Arrays.mapcat(boundariesInElem(elem), function (boundary) {
+			return repeat(boundary, boundaryProbability(boundary));
+		});
+		var index = randomInt(0, boundaries.length - 1);
+		var start = boundaries[index];
+		if (null == distance) {
+			distance = randomInt(0, boundaries.length - 1);
+		}
+		var end = boundaries[Math.min(index + distance(boundaries.length - 1),
+		                              boundaries.length - 1)];
+		var range = Ranges.create();
+		Dom.setRangeFromBoundaries(range, start, end);
+		return range;
+	}
+
+	function defaultBoundaryProbability(mutation, boundary) {
+		return 1;
+	}
+
+	function deleteRangeDistance(totalDistance) {
+		return (0 === randomInt(0, 20)
+		        ? randomInt(0, totalDistance)
+		        : 0);
+	}
+
+	function run(elem, mutations, opts) {
+		var wait = opts.wait || 0;
+		var runs = 0;
+		var timeout;
+		mutations = Arrays.mapcat(mutations, function (mutation) {
+			var probability = mutation.probability;
+			return repeat(mutation, (null != probability ? probability : 1));
+		});
+		function mutate() {
+			if (runs >= opts.runs) {
+				return;
+			}
+			runs += 1;
+			var boundaryProbability
+				= (opts.boundaryProbability
+				   || defaultBoundaryProbability).bind(null, mutation);
+			var mutation = mutations[randomInt(0, mutations.length - 1)];
+			var distance = mutation.deletesRange ? deleteRangeDistance : constantly(0);
+			var range = randomRange(elem, boundaryProbability, distance);
+			mutation.mutate(elem, range);
+			timeout = window.setTimeout(mutate, wait);
+		}
+		timeout = window.setTimeout(mutate, wait);
+		return function () {
+			window.cancelTimeout(timeout);
+		};
+	}
+
+	var exports = {
+		run: run,
+		randomInt: randomInt
+	};
+
+	return exports;
+});
