d7c58321157b38eed4dbd6e4c6234a15d3db8772
Petro Salema <p.salema@gentics.com>
2013-11-16 12:33:00 +0100

    Update module browsers module name

--- src/browsers.js
+++ src/browsers.js
@@ -1,10 +1,10 @@
-/* browser.js is part of Aloha Editor project http://aloha-editor.org
+/* browsers.js is part of Aloha Editor project http://aloha-editor.org
  *
  * Aloha Editor is a WYSIWYG HTML5 inline editing library and editor. 
  * Copyright (c) 2010-2013 Gentics Software GmbH, Vienna, Austria.
  * Contributors http://aloha-editor.org/contribution.php 
  */
-define([], function Browser() {
+define([], function Browsers() {
 	'use strict';
 
 	if ('undefined' !== typeof mandox) {
