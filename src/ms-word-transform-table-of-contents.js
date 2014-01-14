7c021ae1f8c80a2dd11099774acc279a424fd0e4
Petro Salema <p.salema@gentics.com>
2014-01-14 19:42:02 +0100

    Update headers to year 2014

--- src/ms-word-transform-table-of-contents.js
+++ src/ms-word-transform-table-of-contents.js
@@ -1,7 +1,7 @@
 /* ms-word-transform-table-of-contents.js is part of Aloha Editor project http://aloha-editor.org
  *
  * Aloha Editor is a WYSIWYG HTML5 inline editing library and editor.
- * Copyright (c) 2010-2013 Gentics Software GmbH, Vienna, Austria.
+ * Copyright (c) 2010-2014 Gentics Software GmbH, Vienna, Austria.
  * Contributors http://aloha-editor.org/contribution.php
  */
 define([
--- src/strings.js
+++ src/strings.js
@@ -2,7 +2,7 @@
  * strings.js is part of Aloha Editor project http://aloha-editor.org
  *
  * Aloha Editor is a WYSIWYG HTML5 inline editing library and editor.
- * Copyright (c) 2010-2013 Gentics Software GmbH, Vienna, Austria.
+ * Copyright (c) 2010-2014 Gentics Software GmbH, Vienna, Austria.
  * Contributors http://aloha-editor.org/contribution.php
  *
  * Reference:
