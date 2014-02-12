d1956cadd816a47e52edd29e56107436b84cf562
Bob Czarnecki <rczarnecki@constantcontact.com>
2014-02-11 21:32:37 -0500

    PR comment, undo whitespace change

--- src/plugins/common/contenthandler/lib/wordcontenthandler.js
+++ src/plugins/common/contenthandler/lib/wordcontenthandler.js
@@ -245,7 +245,7 @@ define([
 			spans = content.find(detectionFilter);
 			spans.each(function() {
 				var jqElem = jQuery(this),
-						innerText = jQuery.trim(jqElem.text()).replace(/&nbsp;/g, ''),
+				    innerText = jQuery.trim(jqElem.text()).replace(/&nbsp;/g, ''),
 					outerText;
 				
 				if (innerText.length === 0) {
