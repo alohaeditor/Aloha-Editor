fcaa0d50bed9865e52039aa38ccb468544b15a24
Petro Salema <git@petrosalema.com>
2014-02-18 09:41:49 +0100

    Improve ms-word list tests

--- tests/transform/ms-word/lists-tests.js
+++ tests/transform/ms-word/lists-tests.js
@@ -6,6 +6,12 @@
 	var Dom = aloha.dom;
 	var Transform = aloha.transform;
 
+	/**
+	 * Removes trailing white spaces from DOM tree recursively
+	 *
+	 * @param  {Node}
+	 * @return {Node}
+	 */
 	function trim(node) {
 		if (Dom.isTextNode(node)) {
 			node.data = node.data.trim();
@@ -15,16 +21,20 @@
 		return node;
 	}
 
-	function runTests(content) {
+	/**
+	 * Runs a ms-word tranformation test on the given content.
+	 *
+	 * @param  {String} content
+	 */
+	function run(content) {
 		$('<div>' + content + '</div>').find('>.test').each(function () {
 			var $test = $(this);
 			var input = trim($test.find('>.input')[0]).innerHTML;
 			var expected = trim($test.find('>.expected')[0]).innerHTML;
 			var actual = Transform.html(Transform.msword(input, document), document);
-			test(input + ' ⇒ ' + expected, function () {
-				equal(actual, expected);
-			});
+			equal(actual, expected, input + ' ⇒ ' + expected);
 			/*
+			// For debugging
 			$('body')
 				.append('======= input:\n\n\n\n\n\n\n\n\n\n\n\n\n')
 				.append(input)
@@ -37,11 +47,13 @@
 		});
 	}
 
-	for (var i = 1; i <= 16; i++) {
-		$.ajax({
-			async: false,
-			url: 'transform/ms-word/lists/' + (i < 10 ? '0' : '') + i + '.html',
-			success: runTests
-		});
-	}
+	test('Lists', function () {
+		for (var i = 1; i <= 16; i++) {
+			$.ajax({
+				url: 'transform/ms-word/lists/' + (i < 10 ? '0' : '') + i + '.html',
+				async: false,
+				success: run
+			});
+		}
+	});
 }(window.aloha));
