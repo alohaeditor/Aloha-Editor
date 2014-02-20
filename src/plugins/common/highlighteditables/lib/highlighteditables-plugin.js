5e62a01031b0cb0b6de384773eaaf865dbb4132d
Johannes Schüth <j.schueth@gentics.com>
2014-02-20 14:37:57 +0100

    Linted highlighted editables plugin

--- src/plugins/common/highlighteditables/lib/highlighteditables-plugin.js
+++ src/plugins/common/highlighteditables/lib/highlighteditables-plugin.js
@@ -24,79 +24,77 @@
  * provided you include this license notice and a URL through which
  * recipients can access the Corresponding Source.
  */
-define('highlighteditables/highlighteditables-plugin',
-['aloha', 'jquery', 'aloha/plugin', 'aloha/ephemera'],
-function(Aloha, jQuery, Plugin, Ephemera) {
-	"use strict";
+define('highlighteditables/highlighteditables-plugin', ['aloha', 'jquery', 'aloha/plugin', 'aloha/ephemera'],
+	function (Aloha, jQuery, Plugin, Ephemera) {
+		"use strict";
 
-	var
-		GENTICS = window.GENTICS;
+		var
+			GENTICS = window.GENTICS;
 
-	return Plugin.create('highlighteditables', {
+		return Plugin.create('highlighteditables', {
 
-		/**
-		 * default button configuration
-		 */
-		config: [ 'highlight' ],
+			/**
+			 * default button configuration
+			 */
+			config: [ 'highlight' ],
 
-		init: function () {
+			init: function () {
 
-			// remember refernce to this class for callback
-			var that = this,
-				config;
+				// remember refernce to this class for callback
+				var that = this,
+					config;
 
-			Ephemera.classes('aloha-editable-highlight');
+				Ephemera.classes('aloha-editable-highlight');
 
-			// highlight editables as long as the mouse is moving
-			GENTICS.Utils.Position.addMouseMoveCallback(function () {
-				var i,
-					editable;
+				// highlight editables as long as the mouse is moving
+				GENTICS.Utils.Position.addMouseMoveCallback(function () {
+					var i,
+						editable;
 
-				for ( i = 0; i < Aloha.editables.length; i++) {
-					editable = Aloha.editables[i];
-					config = that.getEditableConfig( editable.obj );
+					for (i = 0; i < Aloha.editables.length; i++) {
+						editable = Aloha.editables[i];
+						config = that.getEditableConfig(editable.obj);
 
-					if ( !Aloha.activeEditable && !editable.isDisabled() && config == 'highlight' ) {
-						editable.obj.addClass('aloha-editable-highlight');
+						if (!Aloha.activeEditable && !editable.isDisabled() && config == 'highlight') {
+							editable.obj.addClass('aloha-editable-highlight');
+						}
 					}
-				}
-			});
+				});
 
-			// fade editable borders when mouse stops moving
-			GENTICS.Utils.Position.addMouseStopCallback(function () {
-				that.fade();
-			});
+				// fade editable borders when mouse stops moving
+				GENTICS.Utils.Position.addMouseStopCallback(function () {
+					that.fade();
+				});
 
-			// mark active Editable with a css class
-			Aloha.bind(
-					"aloha-editable-activated",
-					function (jEvent, aEvent) {
-						that.fade();
+				// mark active Editable with a css class
+				Aloha.bind(
+						"aloha-editable-activated",
+						function (jEvent, aEvent) {
+							that.fade();
+						}
+				);
+
+			},
+			/**
+			* fades all highlighted editables
+			*/
+			fade: function () {
+				var
+					i, editable,
+					animateEnd = function () {
+						jQuery(this).css('outline', '');
+					};
+				for (i = 0; i < Aloha.editables.length; i++) {
+					editable = Aloha.editables[i].obj;
+					if (editable.hasClass('aloha-editable-highlight')) {
+						// IE8 fix - hardcode 5px because editable.css('outlineWidth') sometimes causes a javascript error
+						editable.css('outline', editable.css('outlineColor') + ' ' + editable.css('outlineStyle') + ' 5px')
+							.removeClass('aloha-editable-highlight')
+							.animate({
+								outlineWidth : '0px'
+							}, 300, 'swing', animateEnd);
 					}
-			);
-
-		},
-		/**
-		 * fades all highlighted editables
-		 */
-		fade: function () {
-			var
-				i, editable,
-				animateEnd = function () {
-					jQuery(this).css('outline', '');
-				};
-			for ( i = 0; i < Aloha.editables.length; i++) {
-				editable = Aloha.editables[i].obj;
-				if (editable.hasClass('aloha-editable-highlight')) {
-					// IE8 fix - hardcode 5px because editable.css('outlineWidth') sometimes causes a javascript error
-					editable.css('outline', editable.css('outlineColor') + ' ' + editable.css('outlineStyle') + ' 5px')
-						.removeClass('aloha-editable-highlight')
-						.animate({
-							outlineWidth : '0px'
-						}, 300, 'swing', animateEnd);
 				}
 			}
-		}
-
+		});
 	});
-});
