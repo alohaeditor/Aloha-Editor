43fd1fbe191fbb14a246c0ed73f61e85ce34079f
Florian Beer <fb@42dev.eu>
2014-02-20 09:11:53 +0100

    Remove deprecated icons and translations

--- src/plugins/common/list/nls/i18n.js
+++ src/plugins/common/list/nls/i18n.js
@@ -4,21 +4,7 @@ define({
 		"button.createolist.tooltip": "Insert Ordered List",
 		"button.indentlist.tooltip": "Indent List",
 		"button.outdentlist.tooltip": "Outdent List",
-		"floatingmenu.tab.list": "Lists",
-		"button.liststyle.disc": "Disc",
-		"button.liststyle.circle": "Circle",
-		"button.liststyle.square": "Square",
-	    "button.liststyle.decimal": "Decimal",
-	    "button.liststyle.decimal-leading-zero": "Decimal leading zero",
-	    "button.liststyle.lower-roman": "Lowercase roman",
-	    "button.liststyle.upper-roman": "Uppercase roman",
-	    "button.liststyle.lower-greek": "Lowercase greek",
-	    "button.liststyle.lower-latin": "Lowercase latin",
-	    "button.liststyle.upper-latin": "Uppercase latin",
-		"button.liststyle.blue": "Blue",
-		"button.liststyle.green": "Green",
-		"button.liststyle.red": "Red"
-		
+		"floatingmenu.tab.list": "Lists"
 	},
 		"ca": true,
 		"de": true,
