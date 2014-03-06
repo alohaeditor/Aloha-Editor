1cd45e69a9c7b6d3678e16e0364948ac07107fca
Johannes Schüth <j.schueth@gentics.com>
2014-03-06 16:13:13 +0100

    Fix jslint error
    (cherry picked from commit 572f2b05fe997e0aadb7dc840313bf0834bb2b53)

--- src/lib/aloha/markup.js
+++ src/lib/aloha/markup.js
@@ -141,7 +141,7 @@ define([
 	 */
 	function isFrontPosition(node, offset) {
 		if (isTextNode(node)
-			  && offset <= node.data.length - node.data.replace(/^\s+/, '').length) {
+				&& offset <= node.data.length - node.data.replace(/^\s+/, '').length) {
 			return true;
 		}
 
