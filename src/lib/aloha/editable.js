c90b4faf851d47c5c8d85f493eabea93386fa8cb
Petro Salema <p.salema@gentics.com>
2014-04-01 13:58:25 +0200

    Introduce Content Rules for uniform content restriction
    This module is designed as a single point of configuration that will provide a
    contract to uniformly effect editing interactions and the user interface in
    cohesive way.

--- src/lib/aloha/editable.js
+++ src/lib/aloha/editable.js
@@ -1,28 +1,9 @@
-/* editable.js is part of Aloha Editor project http://aloha-editor.org
+/* editable.js is part of the Aloha Editor project http://aloha-editor.org
  *
  * Aloha Editor is a WYSIWYG HTML5 inline editing library and editor.
- * Copyright (c) 2010-2012 Gentics Software GmbH, Vienna, Austria.
+ * Copyright (c) 2010-2014 Gentics Software GmbH, Vienna, Austria.
  * Contributors http://aloha-editor.org/contribution.php
- *
- * Aloha Editor is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or any later version.
- *
- * Aloha Editor is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- * As an additional permission to the GNU GPL version 2, you may distribute
- * non-source (e.g., minimized or compacted) forms of the Aloha-Editor
- * source code without the copy of the GNU GPL normally required,
- * provided you include this license notice and a URL through which
- * recipients can access the Corresponding Source.
+ * License http://aloha-editor.org/license.php
  */
 define([
 	'aloha/core',
@@ -400,7 +381,10 @@ define([
 				 * @param {Array} a an array which contains a reference to the currently created editable on its first position
 				 */
 				Aloha.trigger('aloha-editable-created', [me]);
-				PubSub.pub('aloha.editable.created', {data: me});
+				PubSub.pub('aloha.editable.created', {
+					editable: me,
+					data: me // deprecated
+				});
 			});
 		},
 
@@ -631,7 +615,10 @@ define([
 			 * @param {Array} a an array which contains a reference to the currently created editable on its first position
 			 */
 			Aloha.trigger('aloha-editable-destroyed', [this]);
-			PubSub.pub('aloha.editable.destroyed', {data: this});
+			PubSub.pub('aloha.editable.destroyed', {
+				editable: this,
+				data: this // deprecated
+			});
 
 			// finally register the editable with Aloha
 			Aloha.unregisterEditable(this);
@@ -743,6 +730,9 @@ define([
 				'editable': this
 			});
 			PubSub.pub('aloha.editable.activated', {
+				old: oldActive,
+				editable: this,
+				// deprecated
 				data: {
 					old: oldActive,
 					editable: this
@@ -768,10 +758,10 @@ define([
 			 * @param {Event} e the event object
 			 * @param {Array} a an array which contains a reference to this editable
 			 */
-			Aloha.trigger('aloha-editable-deactivated', {
-				editable: this
-			});
+			Aloha.trigger('aloha-editable-deactivated', {editable: this});
 			PubSub.pub('aloha.editable.deactivated', {
+				editable: this,
+				// deprecated
 				data: {
 					editable: this
 				}
--- src/lib/util/range-context.js
+++ src/lib/util/range-context.js
@@ -1,28 +1,9 @@
-/* range-context.js is part of Aloha Editor project http://aloha-editor.org
+/* range-context.js is part of the Aloha Editor project http://aloha-editor.org
  *
- * Aloha Editor is a WYSIWYG HTML5 inline editing library and editor. 
- * Copyright (c) 2010-2012 Gentics Software GmbH, Vienna, Austria.
- * Contributors http://aloha-editor.org/contribution.php 
- * 
- * Aloha Editor is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or any later version.
- *
- * Aloha Editor is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * 
- * As an additional permission to the GNU GPL version 2, you may distribute
- * non-source (e.g., minimized or compacted) forms of the Aloha-Editor
- * source code without the copy of the GNU GPL normally required,
- * provided you include this license notice and a URL through which
- * recipients can access the Corresponding Source.
+ * Aloha Editor is a WYSIWYG HTML5 inline editing library and editor.
+ * Copyright (c) 2010-2014 Gentics Software GmbH, Vienna, Austria.
+ * Contributors http://aloha-editor.org/contribution.php
+ * License http://aloha-editor.org/license.php
  */
 /**
  * TODO improve restacking and joining algorithm
@@ -37,6 +18,8 @@
  */
 define([
 	'jquery',
+	'aloha/content-rules',
+	'util/dom',
 	'util/dom2',
 	'util/arrays',
 	'util/trees',
@@ -45,6 +28,8 @@ define([
 	'util/html'
 ], function (
 	$,
+	ContentRules,
+	DomLegacy,
 	Dom,
 	Arrays,
 	Trees,
@@ -596,6 +581,10 @@ define([
 				return prevWrapper;
 			}
 			var wrapper = createWrapper();
+			var editable = DomLegacy.getEditingHostOf(node);
+			if (!editable || !ContentRules.isAllowed(editable, wrapper.nodeName)) {
+				return null;
+			}
 			Dom.setStyle(wrapper, styleName, styleValue);
 			wrapAdjust(node, wrapper, leftPoint, rightPoint);
 			removeStyle(node, styleName);
