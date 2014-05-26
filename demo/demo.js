4b74105e6859755f052a3d3f35fa0ca0fa246968
Clemens Prerovsky <c.prerovsky@gentics.com>
2014-05-26 16:02:46 +0200

    added boromir demo

---
+++ demo/demo.js
@@ -0,0 +1,28 @@
+require([
+	'../../src/aloha',
+	'../../src/boundaries'
+], function (
+	aloha,
+	Arrays,
+	Boromir,
+	Boundaries,
+	Dom
+) {
+	'use strict';
+	
+	var caret = document.querySelector('.aloha-caret');
+
+	[].forEach.call(document.querySelectorAll('.aloha-editable'), aloha);
+	
+	window.executeExample = function () {
+		var code = window.event.target.previousElementSibling.innerHTML;
+		window.event.target.blur();
+		eval(code);
+	};
+
+	// properly size the height of all textareas
+	[].forEach.call(document.querySelectorAll('textarea'), function (textarea) {
+		var lines = textarea.innerHTML.split('\n').length;
+		textarea.rows = lines;
+	});
+});
