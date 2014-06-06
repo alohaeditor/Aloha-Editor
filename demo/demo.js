1c723839fb1d5db37fd41af47fd136474dbef0a5
Clemens Prerovsky <c.prerovsky@gentics.com>
2014-06-06 08:58:13 +0200

    styled examples

--- demo/demo.js
+++ demo/demo.js
@@ -8,20 +8,29 @@ require([
 	'use strict';
 	
 	var caret = document.querySelector('.aloha-caret');
+	var codeEditors = {};
+
+	function getExampleName (textarea) {
+		console.log(textarea.className);
+		for (var i=textarea.classList.length; i>=0; i++) {
+			if (textarea.classList[i] !== 'example') {
+				return textarea.classList[i];
+			}
+		}
+	}
 
 	[].forEach.call(document.querySelectorAll('.aloha-editable'), aloha);
 	
-	window.executeExample = function () {
-		var code = window.event.target.previousElementSibling.innerHTML;
+	window.executeExample = function (example) {
+		var code = codeEditors[example + '-code'].getValue();
 		window.event.target.blur();
 		eval(code);
 	};
 
 	// properly size the height of all textareas
 	[].forEach.call(document.querySelectorAll('textarea'), function (textarea) {
-		//var lines = textarea.innerHTML.split('\n').length;
-		//textarea.rows = lines;
-		CodeMirror.fromTextArea(textarea);
+		var exampleName = getExampleName(textarea);
+		codeEditors[exampleName] = CodeMirror.fromTextArea(textarea);
 	});
 
 
