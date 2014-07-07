f92c57e93593b0d12e4d8b13a3a932a84234ff59
Petro Salema <p.salema@gentics.com>
2014-07-07 18:02:58 +0200

    Refactor test

--- tests/content-tests.js
+++ tests/content-tests.js
@@ -1,27 +1,21 @@
-(function (aloha) {
+(function (aloha, module, equal, test) {
 	'use strict';
-
-	var content = aloha.content;
-	var tested = [];
 
     module('content');
 
 	test('allowsNesting', function () {
-		tested.push('allowsNesting');
-		equal(content.allowsNesting('div', 'p'), true);
-		equal(content.allowsNesting('span', 'div'), false);
-		equal(content.allowsNesting('span', 'b'), true);
-		equal(content.allowsNesting('td', 'table'), true);
-		equal(content.allowsNesting('td', 'tr'), false);
-		equal(content.allowsNesting('br', 'span'), false);
-		equal(content.allowsNesting('br', 'br'), false);
-		equal(content.allowsNesting('p', 'p'), false);
-		equal(content.allowsNesting('SPAN', 'span'), true);
-		equal(content.allowsNesting('i', 'i'), true);
-		equal(content.allowsNesting('i', 'table'), false);
-		equal(content.allowsNesting('ol', 'div'), false);
+		equal(aloha.content.allowsNesting('div', 'p'), true);
+		equal(aloha.content.allowsNesting('span', 'div'), false);
+		equal(aloha.content.allowsNesting('span', 'b'), true);
+		equal(aloha.content.allowsNesting('td', 'table'), true);
+		equal(aloha.content.allowsNesting('td', 'tr'), false);
+		equal(aloha.content.allowsNesting('br', 'span'), false);
+		equal(aloha.content.allowsNesting('br', 'br'), false);
+		equal(aloha.content.allowsNesting('p', 'p'), false);
+		equal(aloha.content.allowsNesting('SPAN', 'span'), true);
+		equal(aloha.content.allowsNesting('i', 'i'), true);
+		equal(aloha.content.allowsNesting('i', 'table'), false);
+		equal(aloha.content.allowsNesting('ol', 'div'), false);
 	});
 
-	//testCoverage(test, tested, content);
-
-}(window.aloha));
+}(window.aloha, window.module, window.equal, window.test));
