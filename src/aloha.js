b735e3c97e85f81ef73b58cef79b294bb6ba9991
Petro Salema <p.salema@gentics.com>
2014-07-21 17:00:53 +0200

    Implement aloha.metaview() for debugging

--- src/aloha.js
+++ src/aloha.js
@@ -67,6 +67,7 @@ define([
 	 * @param  {Element} element
 	 */
 	function aloha(element) {
+		Dom.addClass(element, '✪');
 		editor(null, {
 			type         : 'aloha',
 			element      : element,
@@ -89,6 +90,25 @@ define([
 	Api['mahalo'] = mahalo;
 	Api['editor'] = editor;
 	Api['buildcommit'] = '%buildcommit%';
+
+	// cssminimizer.com
+	var METAVIEW_CSS = '.✪{outline:5px solid #fce05e;padding:10px}.✪ td,.✪ th,.✪ b,.✪ i,.✪ u,.✪ p,.✪ ul,.✪ ol,.✪ li,.✪ h1,.✪ h2,.✪ h3,.✪ h4,.✪ h5,.✪ h6,.✪ div{position:relative;padding:2px 4px;margin:2px;border:1px solid rgba(0,0,0,0.1)}.✪ td::before,.✪ th::before,.✪ b::before,.✪ i::before,.✪ u::before,.✪ p::before,.✪ ul::before,.✪ ol::before,.✪ li::before,.✪ h1::before,.✪ h2::before,.✪ h3::before,.✪ h4::before,.✪ h5::before,.✪ h6::before,.✪ div::before{position:absolute;top:0;left:0;line-height:8px;font-size:8px;font-weight:bold;font-style:normal;letter-spacing:0.5px;background:#fff;color:#111}.✪ td::before{content:"TD"}.✪ th::before{content:"TH"}.✪ b::before{content:"B"}.✪ i::before{content:"I"}.✪ u::before{content:"U"}.✪ p::before{content:"P"}.✪ ul::before{content:"UL"}.✪ ol::before{content:"OL"}.✪ li::before{content:"LI"}.✪ h1::before{content:"H1"}.✪ h2::before{content:"H2"}.✪ h3::before{content:"H3"}.✪ h4::before{content:"H4"}.✪ h5::before{content:"H5"}.✪ h6::before{content:"H6"}.✪ div::before{content:"DIV"}.✪ p,.✪ ul,.✪ ol,.✪ h1,.✪ h2,.✪ h3,.✪ h4,.✪ h5,.✪ h6,.✪ div{border-width:2px}.✪ b{border-color:#f47d43}.✪ i{border-color:#82b5e0}.✪ u{border-color:#bb94b7}.✪ ul,.✪ ol{border-color:#91c9cf}.✪ p{border-color:#bdd74b}.✪ h1,.✪ h2,.✪ h3,.✪ h4,.✪ h5,.✪ h6{border-color:#f47d43}';
+
+	/**
+	 * Toggles metaview mode.
+	 */
+	Api['metaview'] = function () {
+		var metaview = doc.querySelector('style#metaview');
+		if (metaview) {
+			Dom.remove(metaview);
+		} else {
+			var text = doc.createTextNode(METAVIEW_CSS);
+			var style = doc.createElement('style');
+			Dom.setAttr(style, 'id', 'metaview');
+			Dom.append(text, style);
+			Dom.append(style, doc.head);
+		}
+	};
 
 	win['aloha'] = aloha = Maps.extend(aloha, Api);
 
