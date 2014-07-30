bfc657d1ec7fe666cdbbcad5b9403c26e9c19d5c
Clemens Prerovsky <c.prerovsky@gentics.com>
2014-07-30 14:14:06 +0200

    simplified build script

--- build.sh
+++ build.sh
@@ -1,18 +1,12 @@
 function help {
 	echo -e "
-\t--s\tSource directory
-\t--o\tOptimization level
-\t--e\tEntry module
-\t--t\tTarget file
+Aloha Editor build script
 
-\tor
+--simple      build with simple optimization - default is advanced
+--source-map  creates a source-mapped output in build directory
 
-\t--simple
-\t--advanced
-\t--source-map creates a source-mapped output in build
-
-\tMake sure to include an .env file in the same directory as this script.
-\tYour .env file should define ClOSURE_PATH=$CLOSURE_PATH.
+Make sure to include an .env file in the same directory as this script.
+Your .env file should define ClOSURE_PATH=$CLOSURE_PATH.
 	"
 }
 
@@ -21,17 +15,28 @@ pwd=$(pwd)
 dot=" \e[0;32m•\e[0m"
 tick=" \e[0;32m✔\e[0m"
 ish=$(echo "https:\/\/github.com\/alohaeditor\/Aloha-Editor\/tree\/$(git rev-parse HEAD)")
+src=src
+entry=aloha
+target=aloha.min.js
+optimization=ADVANCED_OPTIMIZATIONS
 
 function build {
 	if [ -d $pwd/build ]; then
-		printf "$dot Removing old $pwd/build...\n"
+		printf "$dot Removing old $pwd/build\n"
 		rm -r $pwd/build
 	fi
 
-	printf "$dot Creating new $pwd/build...\n"
+	printf "$dot Creating new $pwd/build\n"
 	mkdir $pwd/build
+	
+	if [ "$optimization" == "ADVANCED_OPTIMIZATIONS" ]; then
+		printf "$dot Building with ADVANCED_OPTIMIZATIONS\n"
+	else
+		printf "$dot Building with SIMPLE_OPTIMIZATIONS\n"
+	fi
+
 	cd $src
-	printf "$dot Hang on...\n"
+	
 	versioned=$entry.versioned
 	sed s/%buildcommit%/$ish/ <(cat $entry.js) > $versioned.js
 
@@ -65,63 +70,23 @@ if [ -f .env ]; then
 	source .env
 fi
 
-if [[ $args == "" ]]; then
-	help
-	exit 1
-fi
-
-if [[ $args =~ "--source-map" ]]; then
-	sourcemap="--create_source_map ../build/aloha.js.map --source_map_format=V3"
-else
-	sourcemap=""
-fi
-
-if [[ $args =~ "--advanced" ]]; then
-	src=src
-	entry=aloha
-	target=aloha.min.js
-	optimization=ADVANCED_OPTIMIZATIONS
-	printf "$dot Building with advanced optimizations...\n"
-	build
-	exit 1
-elif [[ $args =~ "--simple" ]]; then
-	src=src
-	entry=aloha
-	target=aloha.min.js
-	optimization=SIMPLE_OPTIMIZATIONS
-	printf "$dot Building with simple optimizations...\n"
-	build
-	exit 1
-fi
-
-if [[ $args =~ "-s=" ]]; then
-	src=$(sed 's/^.*-s=\([^ ]\+\).*$/\1/' <<<"$args")
-else
-	src=.
-fi
-
-if [[ $args =~ "-e=" ]]; then
-	entry=$(sed 's/^.*-e=\([^ ]\+\).*$/\1/' <<<"$args")
-	if [ -f $pwd/$src/$entry".js" ]; then
-		echo ""
-	else
-		echo -e "\n\tFile '$pwd/$src/$entry.js' for entry module does not exist"
-		help
-		exit 1
-	fi
-fi
-
-if [[ $args =~ "-t=" ]]; then
-	target=$(sed 's/^.*-t=\([^ ]\+\).*$/\1/' <<<"$args")
-else
-	target=$(echo "$(date +"%Y-%m-%d").js")
-fi
-
-if [[ $args =~ "-o=" ]]; then
-	optimization=$(sed 's/^.*-o=\([^ ]\+\).*$/\1/' <<<"$args")
-else
-	optimization=SIMPLE_OPTIMIZATIONS
-fi
+while test $# -gt 0; do
+	case "$1" in
+		-h|--help)
+			help
+			exit 0
+			;;
+		--simple)
+			optimization=SIMPLE_OPTIMIZATIONS
+			shift
+			;;
+		--source-map)
+			sourcemap="--create_source_map ../build/aloha.js.map --source_map_format=V3"
+			shift
+			;;
+	esac
+done
 
 build
+exit 1
 
