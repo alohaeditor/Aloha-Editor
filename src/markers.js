80e6c8ffc42ce40f050a3eec6fc22f588885b9ba
Petro Salema <p.salema@gentics.com>
2014-08-07 15:14:42 +0200

    Correct header

--- src/markers.js
+++ src/markers.js
@@ -1,5 +1,5 @@
 /**
- * boundary-markers.js is part of Aloha Editor project http://aloha-editor.org
+ * markers.js is part of Aloha Editor project http://aloha-editor.org
  *
  * Aloha Editor is a WYSIWYG HTML5 inline editing library and editor.
  * Copyright (c) 2010-2014 Gentics Software GmbH, Vienna, Austria.
