19a72f081b0f7acd4a2c66bcf3c74c1c3ae8554a
Petro Salema <p.salema@gentics.com>
2014-09-23 17:13:15 +0200

    Zipper markers

--- src/zippers.js
+++ src/zippers.js
@@ -13,12 +13,14 @@ define([
 	'paths',
 	'arrays',
 	'Boromir',
+	'boundaries',
 	'functions'
 ], function (
 	Dom,
 	Paths,
 	Arrays,
 	Boromir,
+	Boundaries,
 	Fn
 ) {
 	'use strict';
@@ -86,6 +88,9 @@ define([
 
 	function next(loc, stride) {
 		stride = 'number' === typeof stride ? stride : 1;
+		var newLefts = loc.rights.slice(0, stride);
+		console.warn(newLefts.filter(isMarker).length);
+		//stride += newLefts.filter(isMarker).length;
 		return Location(
 			loc.lefts.concat(loc.rights.slice(0, stride)),
 			loc.rights.slice(stride),
@@ -108,16 +113,16 @@ define([
 		);
 	}
 
+	function root(loc) {
+		return loc.frames.reduce(up, loc);
+	}
+
 	function create(root) {
 		return Location([], [Boromir(root)], []);
 	}
 
 	function isTextLocation(loc) {
 		return 'string' === typeof after(loc);
-	}
-
-	function root(loc) {
-		return loc.frames.reduce(up, loc);
 	}
 
 	function update(loc) {
@@ -138,25 +143,175 @@ define([
 		return loc.lefts.map(print).concat('▓', loc.rights.map(print)).join('');
 	}
 
-	function fromBoundary(root, boundary) {
-		var path = Paths.fromBoundary(root, boundary).reverse();
-		var loc = create(root);
-		while (path.length) {
-			var offset = path.pop();
+	function normalizeOffset(record, offset) {
+		var children = record.children();
+		var len = children.length;
+		if (offset >= len) {
+			return offset;
+		}
+		var count = 0;
+		var index = 0;
+		while (index < len) {
+			if (!isMarker(children[index])) {
+				if (count === offset) {
+					return index;
+				}
+				count++;
+			}
+			index++;
+		}
+		return index;
+	}
+
+	function fragmentedOffset(record, offset) {
+		var fragments = record.children();
+		var len = fragments.length;
+		var remainder = offset;
+		var index = 0;
+		var overflow;
+		var fragment;
+		while (index < len) {
+			fragment = fragments[index];
+			if (isMarker(fragment)) {
+				index++;
+				continue;
+			}
+			overflow = remainder - fragment.text().length;
+			if (0 === overflow) {
+				return [index + 1];
+			}
+			if (overflow < 0) {
+				return [index, remainder];
+			}
+			index++;
+			remainder = overflow;
+		}
+		throw 'Text offset out of bounds';
+	}
+
+	function splitText(text, offset) {
+		return [text.substr(0, offset), text.substr(offset)];
+	}
+
+	function locationInText(loc, offset) {
+		var text = splitText(contents(after(loc)), offset);
+		return Location([text[0]], [text[1]], loc.frames.concat(loc));
+	}
+
+	function locationInElement(loc, offset) {
+		return next(down(loc), normalizeOffset(after(loc), offset));
+	}
+
+	function locationInFragment(loc, offset) {
+		var record = after(loc);
+		var offsets = fragmentedOffset(record, offset);
+		var atText = next(down(loc), offsets[0]);
+		if (1 === offsets.length) {
+			return atText;
+		}
+		var text = splitText(contents(after(atText)), offsets[1]);
+		return next(splice(atText, 1, [
+			contents(createRecord('#text'), [text[0]]),
+			contents(createRecord('#text'), [text[1]])
+		]));
+	}
+
+	function traverse(loc, path) {
+		var offset;
+		var trail = path.concat().reverse();
+		while (trail.length) {
+			offset = trail.pop();
 			if (isTextRecord(after(loc))) {
-				var text = contents(after(loc));
-				return Location(
-					[text.substr(0, offset)],
-					[text.substr(offset)],
-					loc.frames.concat(loc)
-				);
+				return locationInText(loc, offset);
 			}
-			loc = down(loc);
-			if (offset > 0) {
-				loc = next(loc, offset);
-			}
+			loc = isFragmentedText(after(loc))
+			    ? locationInFragment(loc, offset)
+			    : locationInElement(loc, offset);
 		}
 		return loc;
+	}
+
+	function clipCommonRoot(root, path) {
+		for (var i = 0; i < root.length; i++) {
+			if (path[i] !== root[i]) {
+				return [];
+			}
+		}
+		return path.slice(i);
+	}
+
+	function splice(loc, num, replacement) {
+		return Location(
+			loc.lefts.concat(),
+			replacement.concat(loc.rights.slice(num)),
+			loc.frames.concat()
+		);
+	}
+
+	function insert(loc, items) {
+		return splice(loc, 0, items);
+	}
+
+	function replace(loc, item) {
+		return splice(loc, 1, [item]);
+	}
+
+	/**
+	 * FragmentedText implementation is backed by a 'Q' element in order to be
+	 * able to visualize it in the document for debugging.
+	 *
+	 * FIXME: isFragmentedText and original won't be preserved on cloning.
+	 */
+	function FragmentedText(loc) {
+		var atText = up(loc);
+		var wrapper = Boromir(document.createElement('q'));
+		wrapper.isFragmentedText = true;
+		wrapper.original = after(atText);
+		return Location(
+			[contents(createRecord('#text'), loc.lefts)],
+			[contents(createRecord('#text'), loc.rights)],
+			down(replace(atText, wrapper)).frames.concat()
+		);
+	}
+
+	function isFragmentedText(record) {
+		return 'Q' === record.name();
+	}
+
+	/**
+	 * FIXME: isFragmentedText and original and isMarker won't be preserved on cloning.
+	 */
+	function Marker() {
+		var node = document.createElement('code');
+		node.innerHTML = '|';
+		var record = Boromir(node);
+		record.isMarker = true;
+		return record;
+	}
+
+	function isMarker(record) {
+		return true === record.isMarker;
+	}
+
+	function mark(loc) {
+		return insert(
+			isTextLocation(loc) ? FragmentedText(loc) : loc,
+			[Marker()]
+		);
+	}
+
+	function insertMarker(loc, path) {
+		return root(mark(traverse(loc, path)));
+	}
+
+	function zipper(element, boundaries) {
+		var body = element.ownerDocument.body;
+		var root = Paths.fromBoundary(body, Boundaries.fromFrontOfNode(element));
+		var paths = (boundaries || []).map(Fn.partial(Paths.fromBoundary, body));
+		var clipped = paths.map(Fn.partial(clipCommonRoot, root)).filter(function (arr) {
+			return arr.length > 0;
+		});
+		return clipped.reduce(insertMarker, create(element));
 	}
 
 	function createRecord(type, content) {
@@ -195,24 +350,31 @@ define([
 		loc = Location(
 			upper.lefts.concat(left),
 			[right].concat(upper.rights.slice(1)),
-			upper.frames
+			upper.frames.concat()
 		);
 		return split(loc, until);
 	}
 
+	setTimeout(function () {
+		var boundaries = Boundaries.get(document);
+		var editable = Dom.editingHost(Boundaries.container(boundaries[0]));
+		var x = zipper(editable, boundaries);
+		update(x);
+	}, 2000);
+
 	return {
-		hint         : hint,
-		update       : update,
-		create       : create,
-		before       : before,
-		after        : after,
-		prev         : prev,
-		next         : next,
-		up           : up,
-		down         : down,
-		root         : root,
-		peek         : peek,
-		split        : split,
-		fromBoundary : fromBoundary
+		hint   : hint,
+		update : update,
+		create : create,
+		before : before,
+		after  : after,
+		prev   : prev,
+		next   : next,
+		up     : up,
+		down   : down,
+		root   : root,
+		peek   : peek,
+		split  : split,
+		zipper : zipper
 	};
 });
