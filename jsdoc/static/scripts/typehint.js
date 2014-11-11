fcc52d7105cde0986eb7f27e63d97e246bb050b2
Clemens Prerovsky <c.prerovsky@gentics.com>
2014-11-11 17:35:05 +0100

    implemented parameter highlighting for the docs fixed double disqus integration

---
+++ jsdoc/static/scripts/typehint.js
@@ -0,0 +1,57 @@
+$(function ($) {
+	var fmatch = document.location.href.match(/#(\w+)/);
+	var tmatch = document.location.href.match(/\?types=([\w-]+)/);
+	if (!(fmatch && tmatch)) {
+		return;
+	}
+	
+	var func  = fmatch[1];
+	var types = tmatch[1].split('-');
+	
+	var $types = $('#' + func).parent().parent().next().find('.type .param-type');
+	var DELAY = 300;
+
+	function compareType (expected, provided) {
+		if (expected === '*') {
+			return true;
+		}
+		if (expected === 'Node' && provided === 'Element') {
+			return true;
+		}
+		if (expected === provided) {
+			return true;
+		}
+		return false;
+	}
+
+	function checkTypes (typesToCheck, i) {
+		i = i || 0;
+		var t = typesToCheck.shift();
+		var $t = $($types.get(i));
+		var expected = $t.text();
+		
+		if (compareType(expected, t)) {
+			$t
+				.addClass('ok')
+				.parent()
+				.prepend('&check; ');
+		} else {
+			$t
+				.addClass('error')
+				.parent()
+				.prepend('&#65794; ')
+				.next()
+				.prepend('<p><b>Info:</b> <i>Expected <span class="param-type error">' +
+					expected +
+					'</span>, you passed <span class="param-type error">' +
+					t +
+					'</span></i></p>');
+		}
+
+		if (typesToCheck.length) {
+			setTimeout(function () { checkTypes(typesToCheck, ++i);	}, DELAY);
+		}
+	}
+	
+	setTimeout(function () { checkTypes(types);	}, DELAY);
+});
