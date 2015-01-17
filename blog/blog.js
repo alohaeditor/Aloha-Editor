19dc75d30f341c5dc44e6d5b411c8c13fbc67b0b
Petro Salema <p.salema@gentics.com>
2015-01-17 23:45:41 +0100

    Add blog styles

---
+++ blog/blog.js
@@ -0,0 +1,95 @@
+(function (aloha, $, CodeMirror) {
+	'use strict';
+
+	var VENDOR_PREFIX = (function () {
+		var elem = document.createElement('div');
+		var prefixes = ['', '-webkit-', '-moz-', '-ms-', '-o-'];
+		var style = elem.style;
+		for (var i = 0; i < prefixes.length; i++) {
+			if (style.hasOwnProperty(prefixes[i] + 'transform')) {
+				return prefixes[i];
+			}
+		}
+		return '';
+	}());
+
+	var $window = $(window);
+
+	var state = {
+		offsets  : [],
+		viewport : {width: 0, height: 0}
+	};
+
+	function onresize() {
+		state.offsets = [];
+		state.viewport = {
+			width  : $window.width(),
+			height : $window.height()
+		};
+		$('.rainbow').each(function (i, elem) {
+			var $elem = $(elem);
+			var offset = $elem.offset().top;
+			state.offsets.push([offset, offset + $elem.height(), $elem.find('.bg')]);
+		});
+	}
+
+	$(function () {
+		if (0 === $('.rainbow .bg').length) {
+			return;
+		}
+		window.requestAnimationFrame(function parallax() {
+			var yStart = $window.scrollTop();
+			var yEnd = yStart + state.viewport.height;
+			state.offsets.forEach(function (offsets) {
+				if (offsets[0] < yEnd && offsets[1] > yStart) {
+					var position = Math.round(yStart - offsets[0]) / 2;
+					offsets[2].css(
+						VENDOR_PREFIX + 'transform',
+						'translate3d(0, ' + position + 'px, 0)'
+					);
+				}
+			});
+			window.requestAnimationFrame(parallax);
+		});
+	});
+
+	function delayed(fn, delay) {
+		var timeout = null;
+		return function () {
+			if (timeout) {
+				clearTimeout(timeout);
+			}
+			timeout = setTimeout(fn, delay);
+		};
+	}
+
+	$window.on('resize', delayed(onresize, 50));
+
+	onresize();
+
+	$(function () {
+		var $items = $('.rainbow');
+		setInterval((function colorcycle() {
+			var colors = [
+				'#54c8eb', // light blue
+				'#4ea9de', // med blue
+				'#4b97d2', // dark blue
+				'#92cc8f', // light green
+				'#41bb98', // mint green
+				//'#c9de83', // yellowish green
+				//'#dee569', // yellowisher green (too light)
+				'#c891c0', // light purple
+				//'#9464a8', // med purple (too light)
+				'#7755a1', // dark purple
+				'#f069a1', // light pink
+				'#f05884', // med pink
+				'#e7457b', // dark pink
+				'#ffd47e', // peach
+				'#f69078'  // salmon
+			];
+			$items.css('background-color', colors[Math.round(Math.random() * (colors.length - 1))]);
+			return colorcycle;
+		}()), 8000);
+	});
+
+}(window.aloha, window.jQuery, window.CodeMirror));
