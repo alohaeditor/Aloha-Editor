99f1f8af61eda266c5ac1a565ced89993b891e20
Petro Salema <p.salema@gentics.com>
2015-01-21 16:47:35 +0100

    Update api docs to use shared  assets

--- jsdoc/static/scripts/main.js
+++ jsdoc/static/scripts/main.js
@@ -66,26 +66,4 @@ $(function () {
             document.getElementsByTagName('BODY')[0].appendChild(s);
         });
     }
-
-	setInterval((function colorcycle() {
-		var colors = [
-			'#54c8eb', // light blue
-			'#4ea9de', // med blue
-			'#4b97d2', // dark blue
-			'#92cc8f', // light green
-			'#41bb98', // mint green
-			//'#c9de83', // yellowish green
-			//'#dee569', // yellowisher green (too light)
-			'#c891c0', // light purple
-			//'#9464a8', // med purple (too light)
-			'#7755a1', // dark purple
-			'#f069a1', // light pink
-			'#f05884', // med pink
-			'#e7457b', // dark pink
-			'#ffd47e', // peach
-			'#f69078'  // salmon
-		];
-		$('.rainbow').css('background-color', colors[Math.round(Math.random() * (colors.length - 1))]);
-		return colorcycle;
-	}()), 8000);
 });
