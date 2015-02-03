90c2c903611a14ef0feb9ee1c8b09d28d40a3ac8
Petro Salema <p.salema@gentics.com>
2015-02-03 17:37:06 +0100

    Update Disqus settings

--- jsdoc/static/scripts/main.js
+++ jsdoc/static/scripts/main.js
@@ -56,14 +56,22 @@ $(function () {
     // disqus code
     if (config.disqus) {
         $(window).on('load', function () {
-            var disqus_shortname = config.disqus; // required: replace example with your forum shortname
-            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-            dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-            var s = document.createElement('script'); s.async = true;
+            var disqus_shortname = config.disqus;
+
+            var d = document.createElement('script');
+			d.async = true;
+			d.type = 'text/javascript';
+            d.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0])
+				.appendChild(d);
+
+            var s = document.createElement('script');
+			s.async = true;
             s.type = 'text/javascript';
             s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
-            document.getElementsByTagName('BODY')[0].appendChild(s);
+
+            document.getElementsByTagName('body')[0].appendChild(s);
         });
     }
 });
